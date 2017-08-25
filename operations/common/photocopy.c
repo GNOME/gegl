735228750f5f672a1a0f914b1d6a1042be25c6f2
Thomas Manni <thomas.manni@free.fr>
2017-08-25 13:22:16 +0200

    gegl:photocopy: use GEGL_OP_FILTER as base class instead of GEGL_OP_AREA_FILTER

--- operations/common/photocopy.c
+++ operations/common/photocopy.c
@@ -36,7 +36,7 @@ property_double (white, _("Percent White"), 0.2)
 
 #else
 
-#define GEGL_OP_AREA_FILTER
+#define GEGL_OP_FILTER
 #define GEGL_OP_NAME     photocopy
 #define GEGL_OP_C_SOURCE photocopy.c
 
