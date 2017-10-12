f0225829e23e4d68ffbb58a490069bc081d34700
Øyvind Kolås <pippin@gimp.org>
2017-10-12 21:09:29 +0200

    reinhard05: update reference hash

--- operations/common/reinhard05.c
+++ operations/common/reinhard05.c
@@ -273,7 +273,7 @@ gegl_op_class_init (GeglOpClass *klass)
   "name",      "gegl:reinhard05",
   "title",      _("Reinhard 2005 Tone Mapping"),
   "categories" , "tonemapping",
-  "reference-hash", "82d46893705823deabfe2511f0561b68",
+  "reference-hash", "9b9792e2b12f6888732c1f16e835e389",
   "description",
         _("Adapt an image, which may have a high dynamic range, for "
           "presentation using a low dynamic range. This is an efficient "
