cac20501098f70d805d96eaca2671b19a5759524
Thomas Manni <thomas.manni@free.fr>
2017-10-16 21:08:57 +0200

    video-degradation: replace underscores by hyphens in enum value strings...
    ...for pattern property, keeping property enumerations consistent between ops.

--- operations/common-gpl3+/video-degradation.c
+++ operations/common-gpl3+/video-degradation.c
@@ -32,17 +32,17 @@
 enum_start (gegl_video_degradation_type)
   enum_value (GEGL_VIDEO_DEGRADATION_TYPE_STAGGERED, "staggered",
               N_("Staggered"))
-  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LARGE_STAGGERED, "large_staggered",
+  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LARGE_STAGGERED, "large-staggered",
               N_("Large staggered"))
   enum_value (GEGL_VIDEO_DEGRADATION_TYPE_STRIPED, "striped",
               N_("Striped"))
-  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_WIDE_STRIPED, "wide_striped",
+  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_WIDE_STRIPED, "wide-striped",
               N_("Wide striped"))
-  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LONG_STAGGERED, "long_staggered",
+  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LONG_STAGGERED, "long-staggered",
               N_("Long staggered"))
   enum_value (GEGL_VIDEO_DEGRADATION_TYPE_3X3, "3x3",
               N_("3x3"))
-  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LARGE_3X3, "large_3x3",
+  enum_value (GEGL_VIDEO_DEGRADATION_TYPE_LARGE_3X3, "large-3x3",
               N_("Large 3x3"))
   enum_value (GEGL_VIDEO_DEGRADATION_TYPE_Hex, "hex",
               N_("Hex"))
