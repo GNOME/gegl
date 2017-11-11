d8ef620c32c751086dc875cefb2b12b9f5ef6d53
Øyvind Kolås <pippin@gimp.org>
2017-11-11 21:31:40 +0100

    grey: update reference hash

--- operations/common/grey.c
+++ operations/common/grey.c
@@ -142,7 +142,7 @@ gegl_op_class_init (GeglOpClass *klass)
       "compat-name", "gegl:grey",
       "title",       _("Make Grey"),
       "categories" , "grayscale:color",
-      "reference-hash", "43ddd80572ab34095298ac7c36368b0c",
+      "reference-hash", "a101c74702f52e4bd2fda04eda56034e",
       "description", _("Turns the image grayscale"),
       NULL);
 }
