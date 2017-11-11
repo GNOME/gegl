471552332c01cf66c3652d6c5c4b43870f0b3017
Øyvind Kolås <pippin@gimp.org>
2017-11-11 21:51:47 +0100

    unsharp-mask: update reference hash

--- operations/common/unsharp-mask.c
+++ operations/common/unsharp-mask.c
@@ -83,7 +83,7 @@ gegl_op_class_init (GeglOpClass *klass)
     "name",        "gegl:unsharp-mask",
     "title",       _("Unsharp Mask"),
     "categories",  "enhance:sharpen",
-    "reference-hash", "9868efefcaf96dddb6ea7e9f8388a717",
+    "reference-hash", "ecedf51ab754c690b8eecf815105a059",
     "description", _("Sharpen image, by adding difference to blurred image, a technique for sharpening originally used in darkrooms."),
     NULL);
 }
