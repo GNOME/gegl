d15522e2564b8cff18908e4349adb9d4e280e3ef
Félix Piédallu <felix@piedallu.me>
2017-11-30 17:31:54 +0100

    Added first draft of meson build

---
+++ tests/opencl/opencl_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+outdir_normal="${ABS_BUILDDIR}/output"
+outdir_opencl="${ABS_BUILDDIR}/output_cl"
+
+mkdir -p "${outdir_normal}"
+mkdir -p "${outdir_opencl}"
+
+GEGL_USE_OPENCL=no  "${GEGL_BIN}" "${XML_FILE}" -o "${outdir_normal}/${OUT_FILE}"
+GEGL_USE_OPENCL=yes "${GEGL_BIN}" "${XML_FILE}" -o "${outdir_opencl}/${OUT_FILE}"
+
+"${GEGL_IMGCMP_BIN}" \
+    "${outdir_normal}/${OUT_FILE}" \
+    "${outdir_opencl}/${OUT_FILE}"
