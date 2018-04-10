8ce10d2aac39438df0c0135bc67c8c7253fbc74a
Øyvind Kolås <pippin@gimp.org>
2018-04-10 16:55:21 +0200

    Revert "panorama-projection: speed up by removing conditionals"
    This reverts commit 09a51b42a63ee88bec35102a727b9535ac7af86b since it
    made the little-planet mode stop working.

--- operations/common/panorama-projection.c
+++ operations/common/panorama-projection.c
@@ -92,6 +92,8 @@ struct _Transform
   float in_height;
   void (*mapfun) (Transform *transform, float x, float  y, float *lon, float *lat);
   int   reverse;
+  int   do_spin;
+  int   do_zoom;
 };
 
 /* formulas from:
@@ -108,10 +110,17 @@ gnomonic_xy2ll (Transform *transform, float x, float y,
   y -= 0.5f;
   x -= transform->xoffset;
 
+  if (transform->do_spin)
   {
     float tx = x, ty = y;
     x = tx * transform->cos_spin - ty * transform->sin_spin;
     y = ty * transform->cos_spin + tx * transform->sin_spin;
+  }
+
+  if (transform->do_zoom)
+  {
+    x /= transform->zoom;
+    y /= transform->zoom;
   }
 
   p = sqrtf (x*x+y*y);
@@ -159,6 +168,12 @@ gnomonic_ll2xy (Transform *transform,
   *y = ((transform->cos_tilt * sin_lat -
          transform->sin_tilt * cos_lat * cos_lon_minus_pan) / cos_c);
 
+  if (transform->do_zoom)
+  {
+    *x *= transform->zoom;
+    *y *= transform->zoom;
+  }
+  if (transform->do_spin)
   {
     float tx = *x, ty = *y;
     *x = tx * transform->cos_negspin - ty * transform->sin_negspin;
@@ -192,6 +207,13 @@ stereographic_ll2xy (Transform *transform,
   *y = k * ((transform->cos_tilt * sin_lat -
         transform->sin_tilt * cos_lat * cos_lon_minus_pan));
 
+  if (transform->do_zoom)
+  {
+    *x *= transform->zoom;
+    *y *= transform->zoom;
+  }
+
+  if (transform->do_spin)
   {
     float tx = *x, ty = *y;
     *x = tx * transform->cos_negspin - ty * transform->sin_negspin;
@@ -214,10 +236,17 @@ stereographic_xy2ll (Transform *transform,
   y -= 0.5f;
   x -= transform->xoffset;
 
+  if (transform->do_spin)
   {
     float tx = x, ty = y;
     x = tx * transform->cos_spin - ty * transform->sin_spin;
     y = ty * transform->cos_spin + tx * transform->sin_spin;
+  }
+
+  if (transform->do_zoom)
+  {
+    x /= transform->zoom;
+    y /= transform->zoom;
   }
 
   p = sqrtf (x*x+y*y);
@@ -283,20 +312,20 @@ static void prepare_transform (Transform *transform,
 
 
 
+  transform->do_spin = fabs (spin) > 0.000001 ? 1 : 0;
+  transform->do_zoom = fabs (zoom-1.0) > 0.000001 ? 1 : 0;
+
   transform->pan         = pan;
   transform->tilt        = tilt;
   transform->spin        = spin;
+  transform->zoom        = zoom;
   transform->xoffset     = xoffset;
   transform->sin_tilt    = sinf (tilt);
   transform->cos_tilt    = cosf (tilt);
-  transform->zoom        = zoom;
-
-
-  transform->sin_spin    = sinf (spin) * zoom;
-  transform->cos_spin    = cosf (spin) * zoom;
-  transform->sin_negspin = sinf (-spin) / zoom;
-  transform->cos_negspin = cosf (-spin) / zoom;
-
+  transform->sin_spin    = sinf (spin);
+  transform->cos_spin    = cosf (spin);
+  transform->sin_negspin = sinf (-spin);
+  transform->cos_negspin = cosf (-spin);
   transform->width       = width;
   transform->height      = height;
   transform->in_width    = input_width;
