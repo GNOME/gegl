GNOME/gegl

commit 03f83003760d1b19de06e08b80143bc70156f45e

gegl: enable multiple thread by default

gegl/gegl-config.c
@@ -48,7 +48,7 @@ enum
   PROP_APPLICATION_LICENSE
 };
 
-gint _gegl_threads = 1; 
+gint _gegl_threads = 1;
 
 static void
 gegl_config_get_property (GObject    *gobject,
@@ -225,11 +225,13 @@ gegl_config_class_init (GeglConfigClass *klass)
                                                         G_PARAM_READWRITE |
                                                         G_PARAM_CONSTRUCT));
 
+  _gegl_threads = g_get_num_processors ();
   g_object_class_install_property (gobject_class, PROP_THREADS,
                                    g_param_spec_int ("threads",
                                                      "Number of threads",
                                                      "Number of concurrent evaluation threads",
-                                                     0, GEGL_MAX_THREADS, 1,
+                                                     0, GEGL_MAX_THREADS,
+                                                     _gegl_threads,
                                                      G_PARAM_READWRITE |
                                                      G_PARAM_CONSTRUCT));
 

commit 7a4fd46392f5e01cdd60bf69cbd993deba1847d6

gegl: Ensure that the "threads" property gets installed on big machines
... where the value of g_get_num_processors exceeds GEGL_MAX_THREADS.

Spotted by Massimo Valentini.

Fallout from 03f83003760d1b19de06e08b80143bc70156f45e

https://gitlab.gnome.org/GNOME/gegl/issues/72

gegl/gegl-config.c
@@ -226,6 +226,7 @@ gegl_config_class_init (GeglConfigClass *klass)
                                                         G_PARAM_CONSTRUCT));
 
   _gegl_threads = g_get_num_processors ();
+  _gegl_threads = MIN (_gegl_threads, GEGL_MAX_THREADS);
   g_object_class_install_property (gobject_class, PROP_THREADS,
                                    g_param_spec_int ("threads",
                                                      "Number of threads",