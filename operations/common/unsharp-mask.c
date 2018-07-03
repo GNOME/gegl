690699bf61e0842ddea576b21121f88895f014a7
Øyvind Kolås <pippin@gimp.org>
2018-07-04 01:56:49 +0200

    unsharp-mask: merge in changes from unsharp-mask-plus

--- operations/common/unsharp-mask.c
+++ operations/common/unsharp-mask.c
@@ -21,14 +21,14 @@
 
 #ifdef GEGL_PROPERTIES
 
-property_double (std_dev, _("Radius"), 0.55)
+property_double (std_dev, _("Radius"), 3.0)
     description(_("Expressed as standard deviation, in pixels"))
     value_range (0.0, 300)
     ui_range    (0.0, 40.0)
     ui_gamma    (3.0)
     ui_meta     ("unit", "pixel-distance")
 
-property_double (scale, _("Amount"), 4.0)
+property_double (scale, _("Amount"), 0.5)
     description(_("Scaling factor for unsharp-mask, the strength of effect"))
     value_range (0.0, 300.0)
     ui_range    (0.0, 10.0)
@@ -47,10 +47,42 @@ property_double (threshold, _("Threshold"), 0.0)
 
 #include "gegl-op.h"
 
+typedef struct
+{
+  GeglNode *aa;
+  GeglNode *absolute;
+  GeglNode *subtract;
+  GeglNode *multiply_mask;
+  GeglNode *multiply;
+} State;
+
+static void
+update_graph (GeglOperation *operation)
+{
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  State *state = o->user_data;
+  if (!state) return;
+  if (o->threshold > 0.0001)
+  {
+    gegl_node_connect_from (state->absolute, "input",
+                            state->subtract, "output");
+    gegl_node_connect_from (state->multiply, "input",
+                            state->multiply_mask, "output");
+  }
+  else
+  {
+    gegl_node_connect_from (state->multiply, "input",
+                            state->subtract,  "output");
+  }
+}
+
 static void
 attach (GeglOperation *operation)
 {
-  GeglNode *gegl, *input, *output, *add, *multiply, *subtract, *blur, *multiply2, *absolute, *threshold, *multiply_mask;
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  GeglNode *gegl, *input, *output, *add, *multiply, *subtract, *blur, *multiply2, *absolute, *threshold, *aa, *multiply_mask;
+  State *state = g_malloc0 (sizeof (State));
+  o->user_data = state;
 
   gegl = operation->node;
 
@@ -63,14 +95,20 @@ attach (GeglOperation *operation)
   subtract = gegl_node_new_child (gegl, "operation", "gegl:subtract", NULL);
   absolute = gegl_node_new_child (gegl, "operation", "gegl:abs", NULL);
   threshold = gegl_node_new_child (gegl, "operation", "gegl:threshold", NULL);
+  aa = gegl_node_new_child (gegl, "operation", "gegl:gaussian-blur", "std-dev-x", 1.0, "std-dev-y", 1.0, NULL);
   blur     = gegl_node_new_child (gegl, "operation", "gegl:gaussian-blur", NULL);
+  state->aa = aa;
+  state->absolute = absolute;
+  state->subtract = subtract;
+  state->multiply = multiply;
+  state->multiply_mask = multiply_mask;
 
   gegl_node_link_many (input, subtract, multiply_mask, multiply, NULL);
   gegl_node_link (input, blur);
   gegl_node_link_many (multiply, add, output, NULL);
 
-  gegl_node_link_many (subtract, absolute, multiply2, threshold, NULL);
-  gegl_node_connect_from (multiply_mask, "aux",  threshold,  "output");
+  gegl_node_link_many (subtract, absolute, multiply2, threshold, aa, NULL);
+  gegl_node_connect_from (multiply_mask, "aux",  aa,  "output");
 
   gegl_node_connect_from (subtract, "aux",   blur,  "output");
   gegl_node_connect_from (add,      "aux",   input, "output");
@@ -81,12 +119,41 @@ attach (GeglOperation *operation)
   gegl_operation_meta_redirect (operation, "std-dev", blur, "std-dev-y");
 
   gegl_operation_meta_watch_nodes (operation, add, multiply, subtract, blur, threshold, NULL);
+
+  update_graph (operation);
+}
+
+static void
+my_set_property (GObject      *object,
+                 guint         property_id,
+                 const GValue *value,
+                 GParamSpec   *pspec)
+{
+  GeglProperties  *o     = GEGL_PROPERTIES (object);
+
+  set_property (object, property_id, value, pspec);
+
+  if (o)
+    update_graph ((void*)object);
+}
+
+static void
+dispose (GObject *object)
+{
+   GeglProperties  *o     = GEGL_PROPERTIES (object);
+   g_clear_pointer (&o->user_data, g_free);
+   G_OBJECT_CLASS (gegl_op_parent_class)->dispose (object);
 }
 
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
+  GObjectClass       *object_class;
   GeglOperationClass *operation_class;
+
+  object_class    = G_OBJECT_CLASS (klass);
+  object_class->dispose      = dispose;
+  object_class->set_property = my_set_property;
 
   operation_class = GEGL_OPERATION_CLASS (klass);
 
