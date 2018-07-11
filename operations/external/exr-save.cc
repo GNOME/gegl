b675cd1e15d7e1e608c855a0bd2e385e6679e9d6
Øyvind Kolås <pippin@gimp.org>
2018-07-11 14:35:12 +0200

    exr-save: fix order of arguments in chromaticities constructor

--- operations/external/exr-save.cc
+++ operations/external/exr-save.cc
@@ -145,10 +145,10 @@ write_tiled_exr (const float       *pixels,
                           &blue[0], &blue[1],
                           NULL, NULL, NULL);
     {
-    Imf::Chromaticities c1 (Imath_2_2::V2f(wp[0],wp[1]),
-                       Imath_2_2::V2f(red[0],red[1]),
-                       Imath_2_2::V2f(green[0],green[1]),
-                       Imath_2_2::V2f(blue[0],blue[1]));
+    Imf::Chromaticities c1 (Imath_2_2::V2f(red[0],red[1]),
+                            Imath_2_2::V2f(green[0],green[1]),
+                            Imath_2_2::V2f(blue[0],blue[1]),
+                            Imath_2_2::V2f(wp[0],wp[1]));
     Imf::addChromaticities (header, c1);
     }
   }
@@ -184,10 +184,10 @@ write_scanline_exr (const float       *pixels,
                            &green[0], &green[1],
                            &blue[0], &blue[1],
                            NULL, NULL, NULL);
-    Imf::Chromaticities c1 (Imath_2_2::V2f(wp[0],wp[1]),
-                       Imath_2_2::V2f(red[0],red[1]),
-                       Imath_2_2::V2f(green[0],green[1]),
-                       Imath_2_2::V2f(blue[0],blue[1]));
+    Imf::Chromaticities c1 (Imath_2_2::V2f(red[0],red[1]),
+                            Imath_2_2::V2f(green[0],green[1]),
+                            Imath_2_2::V2f(blue[0],blue[1]),
+                            Imath_2_2::V2f(wp[0],wp[1]));
     Imf::addChromaticities (header, c1);
   }
 
