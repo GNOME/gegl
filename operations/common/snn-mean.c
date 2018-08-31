b0f1fb52229b1ee1345ad65c3d6e65b7b02f81ca
Øyvind Kolås <pippin@gimp.org>
2018-08-31 15:10:30 +0200

    snn-mean: update reference hash

--- operations/common/snn-mean.c
+++ operations/common/snn-mean.c
@@ -355,7 +355,7 @@ gegl_op_class_init (GeglOpClass *klass)
     "name"       , "gegl:snn-mean",
     "categories" , "enhance:noise-reduction",
     "title",       _("Symmetric Nearest Neighbour"),
-    "reference-hash", "14c0b4b102b1748e4a48555fb8ae9d48",
+    "reference-hash", "dc342e01d1016f92f21d1b2196dc1a7f",
     "description",
         _("Noise reducing edge preserving blur filter based "
           "on Symmetric Nearest Neighbours"),
