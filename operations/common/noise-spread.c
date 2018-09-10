2333dc36cb34907b2f5b415a77078fbb1a9fb82e
Øyvind Kolås <pippin@gimp.org>
2018-09-10 22:36:23 +0200

    noise-spread: use new iterator api

--- operations/common/noise-spread.c
+++ operations/common/noise-spread.c
@@ -21,6 +21,7 @@
  * The workshop/whirl-pinch.c and common/pixelise.c were used as
  * templates for this op file.
  */
+#define GEGL_ITERATOR2_API
 #include "config.h"
 #include <glib/gi18n-lib.h>
 
@@ -115,14 +116,14 @@ process (GeglOperation       *operation,
   bpp = babl_format_get_bytes_per_pixel (format);
 
   gi = gegl_buffer_iterator_new (output, result, 0, format,
-                                 GEGL_ACCESS_WRITE, GEGL_ABYSS_CLAMP);
+                                 GEGL_ACCESS_WRITE, GEGL_ABYSS_CLAMP, 1);
 
   sampler = gegl_buffer_sampler_new_at_level (input, format, GEGL_SAMPLER_NEAREST, level);
 
   while (gegl_buffer_iterator_next (gi))
     {
-      gchar        *data = gi->data[0];
-      GeglRectangle roi  = gi->roi[0];
+      gchar        *data = gi->items[0].data;
+      GeglRectangle roi  = gi->items[0].roi;
       gint          i, j;
 
       for (j = roi.y; j < roi.y + roi.height ; j++)
