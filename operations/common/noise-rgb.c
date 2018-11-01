GNOME/gegl

commit 8681bf105e683aa41bbf132f3ac0989f1d5a6a22

noise-rgb: gummify + fix
When the op's "independent" property is FALSE, only the value of
the "red" property (and not "green" and "blue") affects the output,
using the same noise value for all channels.  Use GUM to hide the
"green" and "blue" properties in this case, and to change the label
of the "red" property to "value".

Fix output when "independent" is FALSE, and "green" or "blue" are
0 (previously, there would be no noise applied to the green/blue
channels, respectively.)

operations/common/noise-rgb.c
@@ -38,12 +38,16 @@ property_boolean (gaussian, _("Gaussian distribution"), TRUE)
 
 property_double (red, _("Red"), 0.20)
    value_range  (0.0, 1.0)
+   ui_meta      ("label", "[! independent : non-independent-label]")
+   ui_meta      ("non-independent-label", _("Value"))
 
 property_double (green, _("Green"), 0.20)
    value_range  (0.0, 1.0)
+   ui_meta      ("visible", "independent")
 
 property_double (blue, _("Blue"), 0.20)
    value_range  (0.0, 1.0)
+   ui_meta      ("visible", "independent")
 
 property_double (alpha, _("Alpha"), 0.0)
    value_range  (0.0, 1.0)
@@ -154,7 +158,7 @@ process (GeglOperation       *operation,
       if (b == 0 || o->independent || b == 3 )
          noise_coeff = noise[b] * noise_fun (o->rand, x, y, &n) * 0.5;
 
-      if (noise[b] > 0.0)
+      if (noise_coeff > 0.0)
       {
         if (o->correlated)
         {

commit 6a2325d1bdad41f97da8b3fbbee307605ef21586

Issue #108 - RGB Noise doesn't produce output on white layer
In gegl:noise-rgb, add noise even when the amount is negative.

This fixes a regression, introduced by commit
8681bf105e683aa41bbf132f3ac0989f1d5a6a22.

operations/common/noise-rgb.c
@@ -163,7 +163,7 @@ process (GeglOperation       *operation,
       if (b == 0 || o->independent || b == 3 )
          noise_coeff = noise[b] * noise_fun (o->rand, x, y, &n) * 0.5;
 
-      if (noise_coeff > 0.0)
+      if (noise_coeff != 0.0)
       {
         if (o->correlated)
         {