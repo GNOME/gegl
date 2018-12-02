GNOME/gegl

commit cc7028d85db62e89bdccaa94704c574414a7da03

buffer: move definitions of GeglRectangle and buffer enums to buffer
This reverts earlier indentation breaking changes when GeglRectangle was
renamed GeglBufferRectangle this change also retains the existing released API
and ABI more exactly.

gegl/gegl-utils.h
@@ -22,250 +22,6 @@
 
 G_BEGIN_DECLS
 
-/***
- * GeglRectangle:
- *
- * GeglRectangles are used in #gegl_node_get_bounding_box and #gegl_node_blit
- * for specifying rectangles.
- *
- * </p><pre>struct GeglRectangle
- * {
- *   gint x;
- *   gint y;
- *   gint width;
- *   gint height;
- * };</pre><p>
- *
- */
-
-/**
- * gegl_rectangle_new:
- * @x: upper left x coordinate
- * @y: upper left y coordinate
- * @width: width in pixels.
- * @height: height in pixels.
- *
- * Creates a new rectangle set with the values from @x, @y, @width and @height.
- */
-GeglRectangle *gegl_rectangle_new        (gint                 x,
-                                          gint                 y,
-                                          guint                width,
-                                          guint                height);
-
-/**
- * gegl_rectangle_set:
- * @rectangle: a #GeglRectangle
- * @x: upper left x coordinate
- * @y: upper left y coordinate
- * @width: width in pixels.
- * @height: height in pixels.
- *
- * Sets the @x, @y, @width and @height on @rectangle.
- */
-void        gegl_rectangle_set           (GeglRectangle       *rectangle,
-                                          gint                 x,
-                                          gint                 y,
-                                          guint                width,
-                                          guint                height);
-
-/**
- * gegl_rectangle_equal:
- * @rectangle1: a #GeglRectangle
- * @rectangle2: a #GeglRectangle
- *
- * Check if two #GeglRectangles are equal.
- *
- * Returns TRUE if @rectangle and @rectangle2 are equal.
- */
-gboolean    gegl_rectangle_equal         (const GeglRectangle *rectangle1,
-                                          const GeglRectangle *rectangle2);
-
-/**
- * gegl_rectangle_equal_coords:
- * @rectangle: a #GeglRectangle
- * @x: X coordinate
- * @y: Y coordinate
- * @width: width of rectangle
- * @height: height of rectangle
- *
- * Check if a rectangle is equal to a set of parameters.
- *
- * Returns TRUE if @rectangle and @x,@y @width x @height are equal.
- */
-gboolean    gegl_rectangle_equal_coords  (const GeglRectangle *rectangle,
-                                          gint                 x,
-                                          gint                 y,
-                                          gint                 width,
-                                          gint                 height);
-
-/**
- * gegl_rectangle_is_empty:
- * @rectangle: a #GeglRectangle
- *
- * Check if a rectangle has zero area.
- *
- * Returns TRUE if @rectangle height and width are both zero.
- */
-gboolean    gegl_rectangle_is_empty     (const GeglRectangle *rectangle);
-
-/**
- * gegl_rectangle_dup:
- * @rectangle: the #GeglRectangle to duplicate
- *
- * Create a new copy of @rectangle.
- *
- * Return value: (transfer full): a #GeglRectangle
- */
-GeglRectangle *gegl_rectangle_dup       (const GeglRectangle *rectangle);
-
-/**
- * gegl_rectangle_copy:
- * @destination: a #GeglRectangle
- * @source: a #GeglRectangle
- *
- * Copies the rectangle information stored in @source over the information in
- * @destination.
- *
- * @destination may point to the same object as @source.
- */
-void        gegl_rectangle_copy          (GeglRectangle       *destination,
-                                          const GeglRectangle *source);
-
-/**
- * gegl_rectangle_bounding_box:
- * @destination: a #GeglRectangle
- * @source1: a #GeglRectangle
- * @source2: a #GeglRectangle
- *
- * Computes the bounding box of the rectangles @source1 and @source2 and stores the
- * resulting bounding box in @destination.
- *
- * @destination may point to the same object as @source1 or @source2.
- */
-void        gegl_rectangle_bounding_box  (GeglRectangle       *destination,
-                                          const GeglRectangle *source1,
-                                          const GeglRectangle *source2);
-
-/**
- * gegl_rectangle_intersect:
- * @dest: return location for the intersection of @src1 and @src2, or NULL.
- * @src1: a #GeglRectangle
- * @src2: a #GeglRectangle
- *
- * Calculates the intersection of two rectangles. If the rectangles do not
- * intersect, dest's width and height are set to 0 and its x and y values
- * are undefined.
- *
- * @dest may point to the same object as @src1 or @src2.
- *
- * Returns TRUE if the rectangles intersect.
- */
-gboolean    gegl_rectangle_intersect     (GeglRectangle       *dest,
-                                          const GeglRectangle *src1,
-                                          const GeglRectangle *src2);
-
-/**
- * gegl_rectangle_subtract_bounding_box:
- * @destination: a #GeglRectangle
- * @minuend: a #GeglRectangle
- * @subtrahend: a #GeglRectangle
- *
- * Computes the bounding box of the area formed by subtracting @subtrahend
- * from @minuend, and stores the result in @destination.
- *
- * @destination may point to the same object as @minuend or @subtrahend.
- *
- * Returns TRUE if the result is not empty.
- */
-gboolean
-    gegl_rectangle_subtract_bounding_box (GeglRectangle       *destination,
-                                          const GeglRectangle *minuend,
-                                          const GeglRectangle *subtrahend);
-
-/**
- * gegl_rectangle_contains:
- * @parent: a #GeglRectangle
- * @child: a #GeglRectangle
- *
- * Checks if the #GeglRectangle @child is fully contained within @parent.
- *
- * Returns TRUE if the @child is fully contained in @parent.
- */
-gboolean    gegl_rectangle_contains      (const GeglRectangle *parent,
-                                          const GeglRectangle *child);
-
-/**
- * gegl_rectangle_infinite_plane:
- *
- * Returns a GeglRectangle that represents an infininte plane.
- */
-GeglRectangle gegl_rectangle_infinite_plane (void);
-
-/**
- * gegl_rectangle_is_infinite_plane:
- * @rectangle: A GeglRectangle.
- *
- * Returns TRUE if the GeglRectangle represents an infininte plane,
- * FALSE otherwise.
- */
-gboolean gegl_rectangle_is_infinite_plane (const GeglRectangle *rectangle);
-
-/**
- * gegl_rectangle_dump:
- * @rectangle: A GeglRectangle.
- *
- * For debugging purposes, not stable API.
- */
-void     gegl_rectangle_dump              (const GeglRectangle *rectangle);
-
-
-/***
- * Aligned memory:
- *
- * GEGL provides functions to allocate and free buffers that are guaranteed to
- * be on 16 byte aligned memory addresses.
- */
-
-/**
- * gegl_malloc: (skip)
- * @n_bytes: the number of bytes to allocte.
- *
- * Allocates @n_bytes of memory. If n_bytes is 0 it returns NULL.
- *
- * Returns a pointer to the allocated memory.
- */
-gpointer gegl_malloc                  (gsize    n_bytes) G_GNUC_MALLOC;
-
-/**
- * gegl_free: (skip)
- * @mem: the memory to free.
- *
- * Frees the memory pointed to by @mem, if @mem is NULL it will warn and abort.
- */
-void     gegl_free                    (gpointer mem);
-
-/**
- * gegl_calloc: (skip)
- * @size: size of items to allocate
- * @n_memb: number of members
- *
- * allocated 0'd memory.
- */
-gpointer gegl_calloc (gsize size, int n_memb) G_GNUC_MALLOC;
-
-/**
- * gegl_memset_pattern: (skip)
- * @dst_ptr: pointer to copy to
- * @src_ptr: pointer to copy from
- * @pattern_size: the length of @src_ptr
- * @count: number of copies
- *
- * Fill @dst_ptr with @count copies of the bytes in @src_ptr.
- */
-void gegl_memset_pattern              (void *       dst_ptr,
-                                       const void * src_ptr,
-                                       gint         pattern_size,
-                                       gint         count);
 
 
 #define GEGL_FLOAT_EPSILON            (1e-5)
@@ -356,9 +112,9 @@ GeglNode *gegl_node_new_from_serialized (const gchar *chaindata,
  *
  * Sets the region covered by rect to the specified color.
  */
-void            gegl_buffer_set_color         (GeglBuffer                *buffer,
-                                               const GeglBufferRectangle *rect,
-                                               GeglColor                 *color);
+void            gegl_buffer_set_color         (GeglBuffer          *buffer,
+                                               const GeglRectangle *rect,
+                                               GeglColor           *color);
 
 
 G_END_DECLS

gegl/Makefile.am
@@ -62,9 +62,11 @@ GEGL_introspectable_headers =	\
 	buffer/gegl-buffer-iterator.h	\
 	buffer/gegl-buffer-iterator2.h	\
 	buffer/gegl-buffer-backend.h	\
+	buffer/gegl-rectangle.h		\
 	buffer/gegl-tile-backend.h		\
 	buffer/gegl-tile-handler.h		\
 	buffer/gegl-tile-source.h		\
+	buffer/gegl-buffer-enums.h		\
 	graph/gegl-node.h			\
 	process/gegl-graph-debug.h		\
 	process/gegl-processor.h		\

gegl/buffer/Makefile.am
@@ -29,12 +29,15 @@ libbuffer_la_SOURCES = \
     gegl-buffer-access.c	\
     gegl-buffer-config.c	\
     gegl-buffer-config.h	\
+    gegl-buffer-enums.c		\
+    gegl-buffer-enums.h		\
     gegl-buffer-matrix2.c	\
     gegl-buffer-matrix2.h	\
     gegl-buffer-index.h		\
     gegl-buffer-iterator.c	\
     gegl-buffer-iterator2.c	\
     gegl-buffer-linear.c	\
+    gegl-rectangle.c	\
 	gegl-buffer-load.c	\
     gegl-buffer-save.c		\
     gegl-sampler.c		\
@@ -64,6 +67,7 @@ libbuffer_la_SOURCES = \
     gegl-buffer-iterator.h	\
     gegl-buffer-iterator2.h	\
     gegl-buffer-iterator-private.h	\
+    gegl-rectangle.h	\
     gegl-buffer-types.h		\
     gegl-buffer-formats.h	\
     gegl-sampler.h		\

gegl/buffer/gegl-algorithms-bilinear.inc
@@ -1,12 +1,12 @@
 void
-BILINEAR_FUNCNAME (guchar              *dest_buf,
-                   const guchar        *source_buf,
+BILINEAR_FUNCNAME (guchar                    *dest_buf,
+                   const guchar              *source_buf,
                    const GeglRectangle *dst_rect,
                    const GeglRectangle *src_rect,
-                   const gint           s_rowstride,
-                   const gdouble        scale,
-                   const gint           bpp,
-                   const gint           d_rowstride)
+                   const gint                 s_rowstride,
+                   const gdouble              scale,
+                   const gint                 bpp,
+                   const gint                 d_rowstride)
 {
   const gint components = bpp / sizeof(BILINEAR_TYPE);
   const gint ver  = s_rowstride/(bpp/components);

gegl/buffer/gegl-algorithms-boxfilter.inc
@@ -1,12 +1,12 @@
 void
-BOXFILTER_FUNCNAME (guchar              *dest_buf,
-                    const guchar        *source_buf,
+BOXFILTER_FUNCNAME (guchar                    *dest_buf,
+                    const guchar              *source_buf,
                     const GeglRectangle *dst_rect,
                     const GeglRectangle *src_rect,
-                    const gint           s_rowstride,
-                    const gdouble        scale,
-                    const gint           bpp,
-                    const gint           d_rowstride)
+                    const gint                 s_rowstride,
+                    const gdouble              scale,
+                    const gint                 bpp,
+                    const gint                 d_rowstride)
 {
   const BOXFILTER_TYPE *src[9];
   gint  components = bpp / sizeof(BOXFILTER_TYPE);

gegl/buffer/gegl-algorithms.c
@@ -26,9 +26,7 @@
 #include <babl/babl.h>
 
 #include "gegl-buffer.h"
-#include "gegl-types.h"
 #include "gegl-buffer-formats.h"
-#include "gegl-utils.h"
 #include "gegl-algorithms.h"
 
 #include <math.h>
@@ -151,14 +149,14 @@ static inline int int_floorf (float x)
 
 
 static void
-gegl_boxfilter_u8_nl (guchar                    *dest_buf,
-                      const guchar              *source_buf,
-                      const GeglBufferRectangle *dst_rect,
-                      const GeglBufferRectangle *src_rect,
-                      const gint                 s_rowstride,
-                      const gdouble              scale,
-                      const gint                 bpp,
-                      const gint                 d_rowstride)
+gegl_boxfilter_u8_nl (guchar              *dest_buf,
+                      const guchar        *source_buf,
+                      const GeglRectangle *dst_rect,
+                      const GeglRectangle *src_rect,
+                      const gint           s_rowstride,
+                      const gdouble        scale,
+                      const gint           bpp,
+                      const gint           d_rowstride)
 {
   const uint8_t *src[9];
   gint  components = bpp / sizeof(uint8_t);
@@ -324,14 +322,14 @@ gegl_boxfilter_u8_nl (guchar                    *dest_buf,
 }
 
 static void
-gegl_boxfilter_u8_nl_alpha (guchar                    *dest_buf,
-                            const guchar              *source_buf,
-                            const GeglBufferRectangle *dst_rect,
-                            const GeglBufferRectangle *src_rect,
-                            const gint                 s_rowstride,
-                            const gdouble              scale,
-                            const gint                 bpp,
-                            const gint                 d_rowstride)
+gegl_boxfilter_u8_nl_alpha (guchar              *dest_buf,
+                            const guchar        *source_buf,
+                            const GeglRectangle *dst_rect,
+                            const GeglRectangle *src_rect,
+                            const gint           s_rowstride,
+                            const gdouble        scale,
+                            const gint           bpp,
+                            const gint           d_rowstride)
 {
   const uint8_t *src[9];
   gint  components = bpp / sizeof(uint8_t);
@@ -457,14 +455,14 @@ gegl_boxfilter_u8_nl_alpha (guchar                    *dest_buf,
 #undef C
 
 static void
-gegl_bilinear_u8_nl (guchar                    *dest_buf,
-                     const guchar              *source_buf,
-                     const GeglBufferRectangle *dst_rect,
-                     const GeglBufferRectangle *src_rect,
-                     const gint                 s_rowstride,
-                     const gdouble              scale,
-                     const gint                 components,
-                     const gint                 d_rowstride)
+gegl_bilinear_u8_nl (guchar              *dest_buf,
+                     const guchar        *source_buf,
+                     const GeglRectangle *dst_rect,
+                     const GeglRectangle *src_rect,
+                     const gint           s_rowstride,
+                     const gdouble        scale,
+                     const gint           components,
+                     const gint           d_rowstride)
 {
   const gint ver  = s_rowstride;
   const gint diag = ver + components;
@@ -585,14 +583,14 @@ gegl_bilinear_u8_nl (guchar                    *dest_buf,
 }
 
 static void
-gegl_bilinear_u8_nl_alpha (guchar                    *dest_buf,
-                           const guchar              *source_buf,
-                           const GeglBufferRectangle *dst_rect,
-                           const GeglBufferRectangle *src_rect,
-                           const gint                 s_rowstride,
-                           const gdouble              scale,
-                           const gint                 components,
-                           const gint                 d_rowstride)
+gegl_bilinear_u8_nl_alpha (guchar              *dest_buf,
+                           const guchar        *source_buf,
+                           const GeglRectangle *dst_rect,
+                           const GeglRectangle *src_rect,
+                           const gint           s_rowstride,
+                           const gdouble        scale,
+                           const gint           components,
+                           const gint           d_rowstride)
 {
   const gint ver  = s_rowstride;
   const gint diag = ver + components;
@@ -993,14 +991,14 @@ gegl_downscale_2x2_nearest (const Babl *format,
 }
 
 static void
-gegl_resample_boxfilter_generic (guchar                    *dest_buf,
-                                 const guchar              *source_buf,
-                                 const GeglBufferRectangle *dst_rect,
-                                 const GeglBufferRectangle *src_rect,
-                                 gint                       s_rowstride,
-                                 gdouble                    scale,
-                                 const Babl                *format,
-                                 gint                       d_rowstride)
+gegl_resample_boxfilter_generic (guchar       *dest_buf,
+                                 const guchar *source_buf,
+                                 const GeglRectangle *dst_rect,
+                                 const GeglRectangle *src_rect,
+                                 gint  s_rowstride,
+                                 gdouble scale,
+                                 const Babl *format,
+                                 gint d_rowstride)
 {
   const Babl *tmp_format = babl_format_with_space ("RGBA float", format);
   const Babl *from_fish  = babl_fish (format, tmp_format);
@@ -1045,14 +1043,14 @@ gegl_resample_boxfilter_generic (guchar                    *dest_buf,
     }
 }
 
-void gegl_resample_boxfilter (guchar                    *dest_buf,
-                              const guchar              *source_buf,
-                              const GeglBufferRectangle *dst_rect,
-                              const GeglBufferRectangle *src_rect,
-                              gint                       s_rowstride,
-                              gdouble                    scale,
-                              const Babl                *format,
-                              gint                       d_rowstride)
+void gegl_resample_boxfilter (guchar              *dest_buf,
+                              const guchar        *source_buf,
+                              const GeglRectangle *dst_rect,
+                              const GeglRectangle *src_rect,
+                              gint                 s_rowstride,
+                              gdouble              scale,
+                              const Babl          *format,
+                              gint                 d_rowstride)
 {
   const Babl *model     = babl_format_get_model (format);
   const Babl *comp_type  = babl_format_get_type (format, 0);
@@ -1100,14 +1098,14 @@ void gegl_resample_boxfilter (guchar                    *dest_buf,
 }
 
 static void
-gegl_resample_bilinear_generic (guchar                    *dest_buf,
-                                const guchar              *source_buf,
-                                const GeglBufferRectangle *dst_rect,
-                                const GeglBufferRectangle *src_rect,
-                                gint                       s_rowstride,
-                                gdouble                    scale,
-                                const Babl                *format,
-                                gint                       d_rowstride)
+gegl_resample_bilinear_generic (guchar              *dest_buf,
+                                const guchar        *source_buf,
+                                const GeglRectangle *dst_rect,
+                                const GeglRectangle *src_rect,
+                                gint                 s_rowstride,
+                                gdouble              scale,
+                                const Babl          *format,
+                                gint                 d_rowstride)
 {
   const Babl *tmp_format = babl_format_with_space ("RGBA float", format);
   const Babl *from_fish  = babl_fish (format, tmp_format);
@@ -1153,14 +1151,14 @@ gegl_resample_bilinear_generic (guchar                    *dest_buf,
     }
 }
 
-void gegl_resample_bilinear (guchar                    *dest_buf,
-                             const guchar              *source_buf,
-                             const GeglBufferRectangle *dst_rect,
-                             const GeglBufferRectangle *src_rect,
-                             gint                       s_rowstride,
-                             gdouble                    scale,
-                             const Babl                *format,
-                             gint                       d_rowstride)
+void gegl_resample_bilinear (guchar              *dest_buf,
+                             const guchar        *source_buf,
+                             const GeglRectangle *dst_rect,
+                             const GeglRectangle *src_rect,
+                             gint                 s_rowstride,
+                             gdouble              scale,
+                             const Babl          *format,
+                             gint                 d_rowstride)
 {
   const Babl *model     = babl_format_get_model (format);
   const Babl *comp_type  = babl_format_get_type (format, 0);
@@ -1210,14 +1208,14 @@ void gegl_resample_bilinear (guchar                    *dest_buf,
 }
 
 void
-gegl_resample_nearest (guchar                    *dst,
-                       const guchar              *src,
-                       const GeglBufferRectangle *dst_rect,
-                       const GeglBufferRectangle *src_rect,
-                       const gint                 src_stride,
-                       const gdouble              scale,
-                       const gint                 bpp,
-                       const gint                 dst_stride)
+gegl_resample_nearest (guchar              *dst,
+                       const guchar        *src,
+                       const GeglRectangle *dst_rect,
+                       const GeglRectangle *src_rect,
+                       const gint           src_stride,
+                       const gdouble        scale,
+                       const gint           bpp,
+                       const gint           dst_stride)
 {
   gint jj[dst_rect->width];
   gint x, y;

commit f9ef0f1c98e1c78f8bb184a414a8f5f3d1fc4450

Silence -Wredundant-decls in GEGL API consumers
Fallout from cc7028d85db62e89bdccaa94704c574414a7da03

gegl/gegl-utils.h
@@ -22,18 +22,6 @@
 
 G_BEGIN_DECLS
 
-
-
-#define GEGL_FLOAT_EPSILON            (1e-5)
-#define GEGL_FLOAT_IS_ZERO(value)     (_gegl_float_epsilon_zero ((value)))
-#define GEGL_FLOAT_EQUAL(v1, v2)      (_gegl_float_epsilon_equal ((v1), (v2)))
-
-/**
- */
-gint        _gegl_float_epsilon_zero  (float     value);
-gint        _gegl_float_epsilon_equal (float     v1,
-                                       float     v2);
-
 typedef enum GeglSerializeFlag {
   GEGL_SERIALIZE_TRIM_DEFAULTS = (1<<0),
   GEGL_SERIALIZE_VERSION       = (1<<1),