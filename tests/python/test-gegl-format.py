0dfd73c1482c68f8252f7602901b70cae3568a44
Félix Piédallu <felix@piedallu.me>
2019-07-11 15:57:40 +0200

    Fix Python 2 -> Python 3 code

--- tests/python/test-gegl-format.py
+++ tests/python/test-gegl-format.py
@@ -36,8 +36,8 @@ class TestGeglFormat(unittest.TestCase):
 
     def test_buffer(self):
       if gi.__version__ in ("3.14.0"):
-        print "SKIPPED! This test is known to be broken in gi version 3.14.0"
-        print "https://bugzilla.gnome.org/show_bug.cgi?id=741291"
+        print("SKIPPED! This test is known to be broken in gi version 3.14.0")
+        print("https://bugzilla.gnome.org/show_bug.cgi?id=741291")
         # This gi version is known to be broken.
         # buf_float.get_property("format") returns an integer,
         # not gobject pointer to the format as it should
