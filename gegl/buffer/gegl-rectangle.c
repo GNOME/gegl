fb9e61dab6db4e22abebf54708935c4fbfd6dbf7
Ell <ell_se@yahoo.com>
2019-07-30 16:02:52 +0300

    buffer: fix silly mistakes in gegl_rectangle_align()

--- gegl/buffer/gegl-rectangle.c
+++ gegl/buffer/gegl-rectangle.c
@@ -90,14 +90,14 @@ gegl_rectangle_align (GeglRectangle          *dest,
 
     case GEGL_RECTANGLE_ALIGNMENT_NEAREST:
       if (x1 > 0) x1 += tile->width  / 2;
-      else        x1 -= tile->width  / 2;
+      else        x1 -= tile->width  / 2 - 1;
       if (x2 > 0) x2 += tile->width  / 2;
-      else        x2 -= tile->width  / 2;
+      else        x2 -= tile->width  / 2 - 1;
 
       if (y1 > 0) y1 += tile->height / 2;
-      else        y1 -= tile->height / 2;
+      else        y1 -= tile->height / 2 - 1;
       if (y2 > 0) y2 += tile->height / 2;
-      else        y2 -= tile->height / 2;
+      else        y2 -= tile->height / 2 - 1;
 
       break;
     }
@@ -113,7 +113,7 @@ gegl_rectangle_align (GeglRectangle          *dest,
       y2 = y2 / tile->height * tile->height;
     }
 
-  if (x1 > x2 && y1 > y2)
+  if (x1 < x2 && y1 < y2)
     {
       if (dest)
         {
