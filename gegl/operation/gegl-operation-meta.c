GNOME/gegl

commit 7a00a24d43a9eb2b98d82d64c19cf8937224517f

gegl: add back stubs for gegl_operation_meta_watch*
These do nothing, by having the stub there the code continues working
but warns at both compile and run-time.

gegl/operation/gegl-operation-meta.c
@@ -72,3 +72,19 @@ gegl_operation_meta_redirect (GeglOperation *operation,
   GeglOperation *internal_operation = gegl_node_get_gegl_operation (internal);
   g_object_bind_property (operation, name, internal_operation, internal_name, G_BINDING_SYNC_CREATE);
 }
+
+void
+gegl_operation_meta_watch_node (GeglOperation     *operation,
+                                GeglNode          *node)
+{
+  g_warning ("%s does nothing\n", __FUNCTION__);
+}
+
+G_DEPRECATED
+void
+gegl_operation_meta_watch_nodes (GeglOperation     *operation,
+                                 GeglNode          *node,
+                                 ...)
+{
+  g_warning ("%s does nothing\n", __FUNCTION__);
+}

gegl/operation/gegl-operation-meta.h
@@ -60,6 +60,19 @@ void  gegl_operation_meta_redirect         (GeglOperation     *operation,
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (GeglOperationMeta, g_object_unref)
 
+G_DEPRECATED
+void  gegl_operation_meta_watch_node       (GeglOperation     *operation,
+                                            GeglNode          *node);
+
+G_DEPRECATED
+void  gegl_operation_meta_watch_nodes      (GeglOperation     *operation,
+                                            GeglNode          *node,
+                                            ...) G_GNUC_NULL_TERMINATED;
+
+
+
+
+
 G_END_DECLS
 
 #endif

commit a157cb1384027ca769bdfec25bcb866b541f5698

operation-meta: Remove a duplicate G_DEPRECATED
It's enough to place it before the function declaration in the header
file, like elsewhere across the codebase.

Fallout from 7a00a24d43a9eb2b98d82d64c19cf8937224517f

gegl/operation/gegl-operation-meta.c
@@ -80,7 +80,6 @@ gegl_operation_meta_watch_node (GeglOperation     *operation,
   g_warning ("%s does nothing\n", __FUNCTION__);
 }
 
-G_DEPRECATED
 void
 gegl_operation_meta_watch_nodes (GeglOperation     *operation,
                                  GeglNode          *node,