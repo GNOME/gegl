11357a56f33baa19116698fd67ab8246162b5d96
Øyvind Kolås <pippin@gimp.org>
2021-02-06 23:47:18 +0100

    gegl: add ctx vector renderer
    A 2D vector graphics rendering engine with support for CMYK and floating
    point raster targets.

---
+++ gegl/ctx/ctx.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <babl/babl.h>
+
+#define CTX_PARSER               1
+#define CTX_FORMATTER            1
+#define CTX_EVENTS               1
+#define CTX_BITPACK_PACKER       0 // turned of due to asan report
+#define CTX_GRADIENT_CACHE       1
+#define CTX_ENABLE_CMYK          1
+#define CTX_ENABLE_CM            1
+#define CTX_RASTERIZER_AA       15
+#define CTX_FORCE_AA             0
+#define CTX_STRINGPOOL_SIZE      10000 // for misc storage with compressed/
+                                       // variable size for each save|restore
+#define CTX_SHAPE_CACHE          0 // when used per gegl op perhsp
+                                  // useful when rendering lots of text initially?
+#define CTX_IMPLEMENTATION 1
+#define CTX_RASTERIZER     1
+
+#include "ctx.h"
