130cd583530dc41adfdec76d6662302f833e6033
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-03-05 12:58:18 +0100

    Fix build with glib 2.67.3 (see https://gitlab.gnome.org/GNOME/glib/-/issues/2331).

--- operations/external/exr-load.cpp
+++ operations/external/exr-load.cpp
@@ -29,9 +29,7 @@ property_file_path (path, "File", "")
 #define GEGL_OP_NAME exr_load
 #define GEGL_OP_C_FILE       "exr-load.cpp"
 
-extern "C" {
 #include "gegl-op.h"
-}
 
 #include <ImfInputFile.h>
 #include <ImfChannelList.h>
