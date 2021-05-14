3d5e9bc8be061c1f52ffe375f06f8e3e15d22f56
JonnyRobbie <marcodv@seznam.cz>
2021-05-14 09:55:24 +0200

    Update, fix and improve negative-darkroom operation * Rework emulsion dye model to (hopefully) properly simulate colors   (this also fixes issue with multigrade paper) * Rework color filter model with UI improvements. * Rework sensitivity matrix calculation to (hopefully) properly   represent paper properties. * Add more black and white papers.

--- operations/common/negative-darkroom.c
+++ operations/common/negative-darkroom.c
@@ -1,5 +1,5 @@
 /* This file is an image processing operation for GEGL
-* foo
+* 
 * GEGL is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
 * License as published by the Free Software Foundation; either
@@ -12,15 +12,12 @@
 *
 * You should have received a copy of the GNU Lesser General Public
 * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
-*
-* Copyright 2015 Red Hat, Inc.
 */
 
 #include "config.h"
 #include <glib/gi18n-lib.h>
 #include <math.h>
 #include <stdio.h>
-
 #ifdef GEGL_PROPERTIES
 
 #include "negative-darkroom/negative-darkroom-curve-enum.c"
@@ -29,50 +26,55 @@ property_enum (curve, _("Characteristic curve"),
 		NegCurve, neg_curve, 0)
 	description(_("Hardcoded characteristic curve and color data"))
 
-property_double (exposure, _("Exposure"), -9.0)
+property_double (exposure, _("Exposure"), 0.0)
 	description(_("Base enlargement exposure"))
-	value_range (-20, 10)
-	ui_range (-15, 0)
+	value_range (-10, 10)
+	ui_range (-5, 5)
 
-property_double (expC, _("Filter cyan"), 0.0)
-	description(_("Cyan exposure compensation for the negative image"))
-	value_range (-3, 3)
+property_double (expC, _("Cyan filter"), 60)
+	description(_("Cyan filter compensation for the negative image"))
+	value_range (0, 300)
+	ui_range (0, 180)
 
-property_double (expM, _("Filter magenta"), 0.0)
-	description(_("Magenta exposure compensation for the negative image"))
-	value_range (-3, 3)
+property_double (expM, _("Magenta filter"), 60)
+	description(_("Magenta filter compensation for the negative image"))
+	value_range (0, 300)
+	ui_range (0, 180)
 
-property_double (expY, _("Filter yellow"), 0.0)
-	description(_("Yellow exposure compensation for the negative image"))
-	value_range (-3, 3)
+property_double (expY, _("Yellow filter"), 60)
+	description(_("Yellow filter compensation for the negative image"))
+	value_range (0, 300)
+	ui_range (0, 180)
 
 property_boolean (clip, _("Clip base + fog"), TRUE)
 	description (_("Clip base + fog to have a pure white output value"))
 
 property_double (boost, _("Density boost"), 1.0)
 	description(_("Boost paper density to take advantage of increased dynamic range of a monitor compared to a photographic paper"))
-	value_range (1, 3)
+	value_range (0.25, 4)
+	ui_range (1, 2)
 
 property_double (dodge, _("Dodge/burn multiplier"), 1.0)
 	description(_("The f-stop of dodge/burn for pure white/black auxillary input"))
 	value_range (-4.0, 4.0)
+	ui_range (0, 2)
 
 property_boolean (preflash, _("Enable preflashing"), FALSE)
 	description (_("Show preflash controls"))
 
-property_double (flashC, _("Cyan preflash"), 0)
-	description(_("Preflash the negative with cyan light to reduce contrast of the print"))
-	value_range (0, 1)
+property_double (flashC, _("Red preflash"), 0)
+	description(_("Preflash the negative with red light to reduce contrast of the print"))
+	value_range (0, 0.05)
 	ui_meta("visible", "preflash")
 
-property_double (flashM, _("Magenta preflash"), 0)
-	description(_("Preflash the negative with magenta light to reduce contrast of the print"))
-	value_range (0, 1)
+property_double (flashM, _("Green preflash"), 0)
+	description(_("Preflash the negative with green light to reduce contrast of the print"))
+	value_range (0, 0.05)
 	ui_meta("visible", "preflash")
 
-property_double (flashY, _("Yellow preflash"), 0)
-	description(_("Preflash the negative with yellow light to reduce contrast of the print"))
-	value_range (0, 1)
+property_double (flashY, _("Blue preflash"), 0)
+	description(_("Preflash the negative with blue light to reduce contrast of the print"))
+	value_range (0, 0.05)
 	ui_meta("visible", "preflash")
 
 #else
@@ -80,6 +82,8 @@ property_double (flashY, _("Yellow preflash"), 0)
 #define GEGL_OP_POINT_COMPOSER
 #define GEGL_OP_NAME     negative_darkroom
 #define GEGL_OP_C_SOURCE negative-darkroom.c
+
+#define EPSILON 0.00001
 
 #include "gegl-op.h"
 
@@ -155,6 +159,12 @@ array_min (gfloat * x, guint n)
 	return(min);
 }
 
+static inline gfloat
+clampE (gfloat x)
+{
+	return(x <= EPSILON ? EPSILON : x);
+}
+
 static gboolean
 process (GeglOperation       *operation,
 	 void                *in_buf,
@@ -182,61 +192,82 @@ process (GeglOperation       *operation,
 	gfloat g = 0;
 	gfloat b = 0;
 
+	gfloat x = 0;
+	gfloat y = 0;
+	gfloat z = 0;
+
+	gfloat exp = pow(2, o->exposure);
+
 	// Calculate base+fog
 	if (o->clip)
 	{
-		Dfogc = array_min(curves[o->curve].ry, curves[o->curve].rn) * o->boost;
-		Dfogm = array_min(curves[o->curve].gy, curves[o->curve].gn) * o->boost;
-		Dfogy = array_min(curves[o->curve].by, curves[o->curve].bn) * o->boost;
+		Dfogc = array_min(curves[o->curve].ry,
+				  curves[o->curve].rn) * o->boost;
+		Dfogm = array_min(curves[o->curve].gy,
+				  curves[o->curve].gn) * o->boost;
+		Dfogy = array_min(curves[o->curve].by,
+				  curves[o->curve].bn) * o->boost;
+	}
+
+	if (!aux)
+	{
+		rcomp = pow(2, (-o->expC) / 30);
+		gcomp = pow(2, (-o->expM) / 30);
+		bcomp = pow(2, (-o->expY) / 30);
 	}
 
 	for (glong i = 0; i < n_pixels; i++)
 	{
-		/*printf("---\n");*/
 		/*printf("Input XYZ intensity %f %f %f\n", in[0], in[1], in[2]);*/
 
 		// Calculate exposure compensation from global+filter+dodge
-		if (!aux)
+		if (aux)
 		{
-			rcomp = o->exposure + o->expC;
-			gcomp = o->exposure + o->expM;
-			bcomp = o->exposure + o->expY;
-		}
-		else
-		{
-			rcomp = o->exposure + o->expC + 2*o->dodge*(aux[0]-0.5);
-			gcomp = o->exposure + o->expM + 2*o->dodge*(aux[1]-0.5);
-			bcomp = o->exposure + o->expY + 2*o->dodge*(aux[2]-0.5);
+			rcomp = pow(2, ((-o->expC) / 30) -
+					(2 * o->dodge * (aux[0] - 0.5)));
+			gcomp = pow(2, ((-o->expM) / 30) -
+					(2 * o->dodge * (aux[1] - 0.5)));
+			bcomp = pow(2, ((-o->expY) / 30) -
+					(2 * o->dodge * (aux[2] - 0.5)));
 			aux  += 3;
 		}
 
-		/*printf("===\nRGB compensation %f %f %f\n", rcomp, gcomp, bcomp);*/
+		// Convert to CIERGB primaries for color filter balance
+		x =    0.41847*in[0] -   0.15866*in[1] - 0.082835*in[2];
+		y =  -0.091169*in[0] +   0.25243*in[1] + 0.015708*in[2];
+		z = 0.00092090*in[0] - 0.0025498*in[1] +  0.17860*in[2];
 
-		// Calculate RGB from XYZ using paper sensitivity
-		r = in[0] * curves[o->curve].rsens.X +
-		    in[1] * curves[o->curve].gsens.X +
-		    in[2] * curves[o->curve].bsens.X;
-		g = in[0] * curves[o->curve].rsens.Y +
-		    in[1] * curves[o->curve].gsens.Y +
-		    in[2] * curves[o->curve].bsens.Y;
-		b = in[0] * curves[o->curve].rsens.Z +
-		    in[1] * curves[o->curve].gsens.Z +
-		    in[2] * curves[o->curve].bsens.Z;
+		// Apply preflash
+		x = x + o->flashC;
+		y = y + o->flashM;
+		z = z + o->flashY;
 
-		/*printf("Linear RGB intensity %f %f %f\n", r, g, b);*/
+		// Apply color filters and exposure
+		x = x * rcomp * exp;
+		y = y * gcomp * exp;
+		z = z * bcomp * exp;
 
-		// Apply the preflash
-		r = r + o->flashC;
-		g = g + o->flashM;
-		b = b + o->flashY;
+		// Simulate emulsion spectral sensitivity with
+		// sensitivity matrix
+		r = clampE(x * curves[o->curve].rsens.X +
+			   y * curves[o->curve].rsens.Y +
+			   z * curves[o->curve].rsens.Z);
+		g = clampE(x * curves[o->curve].gsens.X +
+			   y * curves[o->curve].gsens.Y +
+			   z * curves[o->curve].gsens.Z);
+		b = clampE(x * curves[o->curve].bsens.X +
+			   y * curves[o->curve].bsens.Y +
+			   z * curves[o->curve].bsens.Z);
 
-		/*printf("Linear RGB intensity after preflash %f %f %f\n", r, g, b);*/
+		// Scale the emulsion response
+		r *= 5000;
+		g *= 5000;
+		b *= 5000;
 
-		// Logarithmize the input and apply compensation
-		r = log(r / pow(2, rcomp)) / log(10);
-		g = log(g / pow(2, gcomp)) / log(10);
-		b = log(b / pow(2, bcomp)) / log(10);
-
+		// Logarithmize the input
+		r = log10(r);
+		g = log10(g);
+		b = log10(b);
 		/*printf("Logarithmic RGB intensity %f %f %f\n", r, g, b);*/
 
 		// Apply the DH curve
@@ -252,6 +283,7 @@ process (GeglOperation       *operation,
 			       curves[o->curve].by,
 			       curves[o->curve].bn,
 			       b);
+		/*printf("Raw RGB density %f %f %f\n", r, g, b);*/
 
 		// Apply density boost
 		r = r * o->boost;
@@ -262,27 +294,19 @@ process (GeglOperation       *operation,
 		r -= Dfogc;
 		g -= Dfogm;
 		b -= Dfogy;
+		/*printf("Adjusted RGB density %f %f %f\n", r, g, b);*/
 
-		/*printf("RGB density %f %f %f\n", r, g, b);*/
-
-		// Exponentiate to get the linear representation in tramsmittance back
-		r = 1-1/pow(10, r);
-		g = 1-1/pow(10, g);
-		b = 1-1/pow(10, b);
-
-		/*printf("RGB absolute transparency %f %f %f\n", r, g, b);*/
-
-		// Calculate and return XYZ
-		out[0] = 1 - (r * curves[o->curve].cdens.X) -
-			     (g * curves[o->curve].mdens.X) -
-			     (b * curves[o->curve].ydens.X);
-		out[1] = 1 - (r * curves[o->curve].cdens.Y) -
-			     (g * curves[o->curve].mdens.Y) -
-			     (b * curves[o->curve].ydens.Y);
-		out[2] = 1 - (r * curves[o->curve].cdens.Z) -
-			     (g * curves[o->curve].mdens.Z) -
-			     (b * curves[o->curve].ydens.Z);
-
+		// Simulate dye density with exponentiation to get
+		// the CIEXYZ tramsmittance back
+		out[0] = (1 / pow(10, r * curves[o->curve].cdens.X)) *
+			 (1 / pow(10, g * curves[o->curve].mdens.X)) *
+			 (1 / pow(10, b * curves[o->curve].ydens.X));
+		out[1] = (1 / pow(10, r * curves[o->curve].cdens.Y)) *
+			 (1 / pow(10, g * curves[o->curve].mdens.Y)) *
+			 (1 / pow(10, b * curves[o->curve].ydens.Y));
+		out[2] = (1 / pow(10, r * curves[o->curve].cdens.Z)) *
+			 (1 / pow(10, g * curves[o->curve].mdens.Z)) *
+			 (1 / pow(10, b * curves[o->curve].ydens.Z));
 		/*printf("XYZ output %f %f %f\n", out[0], out[1], out[2]);*/
 
 		in   += 3;
@@ -310,9 +334,10 @@ gegl_op_class_init (GeglOpClass *klass)
 	gegl_operation_class_set_keys (operation_class,
 		"name",           "gegl:negative-darkroom",
 		"title",          _("Negative Darkroom"),
-		"categories",     "color",
+		"categories" ,    "color",
 		"reference-hash", "d492ac8ef38c336aa79e63b7f39f6139",
-		"description", _("Simulate a negative film enlargement in an analog darkroom."),
+		"description",    _("Simulate a negative film enlargement in "
+				  "an analog darkroom."),
 		NULL);
 }
 
