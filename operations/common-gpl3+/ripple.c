2e1e4112d671ef60533be0a97aeaed67ad83badb
Jehan <jehan@girinstud.io>
2021-09-05 20:13:42 +0200

    operations: fix "fatal error: Floating point exception".
    div() expects integer arguments, and therefore any denominator less than
    1.0 gets rounded to 0, hence doing a division by 0.
    
    Instead remainder() works on double values, and anyway we only needed
    the remainder.
    
    See gimp#6888.
    Thanks to Massimo for reporting the source of the issue.

--- operations/common-gpl3+/ripple.c
+++ operations/common-gpl3+/ripple.c
@@ -200,14 +200,14 @@ process (GeglOperation       *operation,
             switch (o->wave_type)
               {
                 case GEGL_RIPPLE_WAVE_TYPE_SAWTOOTH:
-                  lambda = div (nx + period / 2 - phi * period, period).rem;
+                  lambda = remainder (nx + period / 2 - phi * period, period);
                   if (lambda < 0)
                     lambda += period;
                   shift = amplitude * (((lambda / period) * 2) - 1);
                   break;
 
                 case GEGL_RIPPLE_WAVE_TYPE_TRIANGLE:
-                  lambda = div (nx + period * 3 / 4 - phi * period, period).rem;
+                  lambda = remainder (nx + period * 3 / 4 - phi * period, period);
                   if (lambda < 0)
                     lambda += period;
                   shift = amplitude * (fabs (((lambda / period) * 4) - 2) - 1);
