712e40b7774586c451721b6132a4af41c17b700e
Øyvind Kolås <pippin@gimp.org>
2023-02-20 23:07:34 +0100

    rgb-clip: fix issue #316, better ui-ranges

--- operations/common/rgb-clip.c
+++ operations/common/rgb-clip.c
@@ -27,7 +27,7 @@ property_boolean (clip_low, _("Clip low pixel values"), TRUE)
 
 property_double (low_limit, _("Low limit"), 0.0)
     value_range (-G_MAXDOUBLE, G_MAXDOUBLE)
-    ui_range    (-2.0, 0.0)
+    ui_range    (-1.0, 1.0)
     description (_("Pixels values lower than this limit will be set to it"))
     ui_meta     ("sensitive", "clip-low")
 
@@ -36,7 +36,7 @@ property_boolean (clip_high, _("Clip high pixel values"), TRUE)
 
 property_double (high_limit, _("High limit"), 1.0)
     value_range (-G_MAXDOUBLE, G_MAXDOUBLE)
-    ui_range    (1.0, 3.0)
+    ui_range    (0.0, 2.0)
     description (_("Pixels values higher than this limit will be set to it"))
     ui_meta     ("sensitive", "clip-high")
 
