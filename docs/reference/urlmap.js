239e060d71fc3a668374efecb0b173eee67eb381
Jehan <jehan@girinstud.io>
2023-08-12 17:07:19 +0200

    docs: adding gi-docgen support.

---
+++ docs/reference/urlmap.js
@@ -0,0 +1,7 @@
+// A map between namespaces and base URLs for their online gi-docgen documentation
+baseURLs = [
+  [ 'Babl',    'https://developer.gimp.org/api/babl/' ],
+  [ 'GLib',    'https://docs.gtk.org/glib/' ],
+  [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+  [ 'Gio',     'https://docs.gtk.org/gio/' ],
+]
