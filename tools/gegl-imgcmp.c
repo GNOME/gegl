GNOME/gegl

commit 40087f6d3ad64a0156ecb30cc890c4a097ac3c49

gegl, operations, tools: Complete some faulting Windows functions

tools/gegl-imgcmp.c
@@ -3,6 +3,11 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#if !defined (HAVE_UNISTD_H) && defined(_WIN64)
+#include <io.h>
+#define access _access
+#define F_OK 0
+#endif
 
 #include <gegl.h>
 #include <glib.h>

gegl/buffer/gegl-buffer-load.c
@@ -27,6 +27,12 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#if !defined(HAVE_UNISTD_H) && defined(_WIN64)
+#include <io.h>
+#define read _read
+#define close _close
+#define lseek _lseek
+#endif
 
 #include "gegl-buffer.h"
 #include "gegl-buffer-private.h"
@@ -36,8 +42,10 @@
 #include <glib/gprintf.h>
 #include <glib/gstdio.h>
 
-#ifdef G_OS_WIN32
+#ifdef _WIN64
 #define BINARY_FLAG O_BINARY
+#include <basetsd.h>
+typedef SSIZE_T ssize_t;
 #else
 #define BINARY_FLAG 0
 #endif

gegl/buffer/gegl-buffer-save.c
@@ -26,6 +26,11 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#if !defined(HAVE_UNISTD_H) && defined(_WIN64)
+#include <io.h>
+#define write _write
+#define close _close
+#endif
 #include <errno.h>
 
 #include <glib-object.h>
@@ -39,8 +44,19 @@
 #include "gegl-tile.h"
 #include "gegl-buffer-index.h"
 
-#ifdef G_OS_WIN32
+#ifdef _WIN64
 #define BINARY_FLAG O_BINARY
+#include <basetsd.h>
+typedef SSIZE_T ssize_t;
+#ifndef S_IRUSR
+#define S_IRUSR _S_IREAD
+#endif
+#ifndef S_IWUSR
+#define S_IWUSR _S_IWRITE
+#endif
+#ifndef S_IXUSR
+#define S_IXUSR 0
+#endif
 #else
 #define BINARY_FLAG 0
 #endif

gegl/buffer/gegl-buffer-swap.c
@@ -30,6 +30,15 @@
 #include <windows.h>
 #include <process.h>
 #define getpid() _getpid()
+#ifndef S_IRUSR
+#define S_IRUSR _S_IREAD
+#endif
+#ifndef S_IWUSR
+#define S_IWUSR _S_IWRITE
+#endif
+#ifndef S_IXUSR
+#define S_IXUSR 0
+#endif
 #else
 #include <signal.h>
 #endif

gegl/buffer/gegl-tile-backend-file-async.c
@@ -39,6 +39,14 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#if !defined (HAVE_UNISTD_H) && defined(_WIN64)
+#include <io.h>
+#define read _read
+#define write _write
+#define close _close
+#define lseek _lseek
+#define ftruncate _chsize_s
+#endif
 #include <string.h>
 #include <errno.h>
 
@@ -67,6 +75,7 @@
 
 #ifdef G_OS_WIN32
 #define BINARY_FLAG O_BINARY
+#define F_OK 0
 #else
 #define BINARY_FLAG 0
 #endif

gegl/buffer/gegl-tile-backend-swap.c
@@ -23,6 +23,14 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#if !defined (HAVE_UNISTD_H) && defined(_WIN64)
+#include <io.h>
+#define read _read
+#define write _write
+#define close _close
+#define lseek _lseek
+#define ftruncate _chsize_s
+#endif
 #include <string.h>
 #include <errno.h>
 

gegl/gegl-init.c
@@ -36,6 +36,15 @@
 #ifdef G_OS_WIN32
 
 #include <windows.h>
+#ifndef S_IRUSR
+#define S_IRUSR _S_IREAD
+#endif
+#ifndef S_IWUSR
+#define S_IWUSR _S_IWRITE
+#endif
+#ifndef S_IXUSR
+#define S_IXUSR 0
+#endif
 
 static HMODULE hLibGeglModule = NULL;
 

gegl/opencl/gegl-cl-init.c
@@ -31,6 +31,9 @@
 #include <glib.h>
 #include <gmodule.h>
 #include <string.h>
+#ifdef _WIN64
+#define strdup _strdup
+#endif
 #include <stdio.h>
 
 #include "gegl-cl.h"

commit 6cf9ab9c4954b389e381c145bd853a1fb4d00750

tools: Revert fa8eb2c4
Proper fix to bug introduced by 40087f6d

tools/gegl-imgcmp.c
@@ -1,3 +1,4 @@
+#include "config.h"
 #include <errno.h>
 #include <string.h>
 #ifdef HAVE_UNISTD_H

tools/meson.build
@@ -6,7 +6,6 @@ tools_deps = [
 ]
 
 tools_c_args = [
-  cflags_common,
   '-DTOP_SRCDIR="' + project_source_root + '"',
 ]
 