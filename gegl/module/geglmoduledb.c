cf49d2fcca7b456506951ddaa492551127fb70c9
Bruno Lopes <brunvonlope@outlook.com>
2025-11-23 13:49:32 -0300

    gegl/module: G_MODULE_SUFFIX is deprecated

--- gegl/module/geglmoduledb.c
+++ gegl/module/geglmoduledb.c
@@ -33,10 +33,13 @@
 #define ARCH_SIMD
 #endif
 
-#ifdef __APPLE__ /* G_MODULE_SUFFIX is defined to .so instead of .dylib */
-#define MODULE_SUFFIX "dylib"
+ /* G_MODULE_SUFFIX is deprecated */
+#if !defined(_WIN32) && !defined(__APPLE__)
+#define MODULE_SUFFIX "so"
+#elif defined(_WIN32)
+#define MODULE_SUFFIX "dll"
 #else
-#define MODULE_SUFFIX G_MODULE_SUFFIX
+#define MODULE_SUFFIX "dylib"
 #endif
 
 enum
@@ -337,10 +340,10 @@ valid_module_name (const gchar *filename)
           return FALSE;
         }
     }
-#ifdef __APPLE__ /* G_MODULE_SUFFIX is defined to .so instead of .dylib */
+#ifdef __APPLE__
   if (! g_str_has_suffix (basename, ".dylib" ) || strstr (filename, ".dSYM"))
 #else
-  if (! g_str_has_suffix (basename, "." G_MODULE_SUFFIX))
+  if (! g_str_has_suffix (basename, "." MODULE_SUFFIX))
 #endif
     {
       g_free (basename);
