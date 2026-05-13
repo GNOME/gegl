c65b88e18d25df35615cadf58b832b2cee5c59c7
WarisMaqbool <warisk709@gmail.com>
2026-05-13 13:25:24 +0530

    Add iterator for input and remove the kernel loop

--- operations/workshop/sharpen.c
+++ operations/workshop/sharpen.c
@@ -13,7 +13,7 @@ property_double (amount, _("Amount"), 10.0)
 
 #else
 
-#define GEGL_OP_FILTER
+#define GEGL_OP_AREA_FILTER
 #define GEGL_OP_NAME     sharpen
 #define GEGL_OP_C_SOURCE sharpen.c
 
@@ -24,8 +24,6 @@ prepare (GeglOperation *operation)
 {
   const Babl *space = gegl_operation_get_source_space (operation, "input");
 
-  gegl_operation_set_format (operation, "input",
-                             babl_format_with_space ("R'G'B'A float", space));
   gegl_operation_set_format (operation, "output",
                              babl_format_with_space ("R'G'B'A float", space));
 }
@@ -37,86 +35,131 @@ process (GeglOperation       *op,
          const GeglRectangle *roi,
          gint                 level)
 {
-  GeglProperties     *o            = GEGL_PROPERTIES (op);
-  const Babl         *format       = babl_format_with_space ("R'G'B'A float",
-                                                             gegl_operation_get_source_space (op, "input"));
-  
-  gfloat              percentage   = o->amount;
-  gfloat              fact         = 1.0f - (percentage / 100.0f);
-  gint                channels_num = 4;
-  
-  /* Clamping fact to avoid division by zero */ 
-  if (fact < 0.01f) fact = 0.01f;
-
-  /* In the legacy version, the center was (800/fact) and neighbors were (pos - i*8)/8.
-   * In normalized float math, we set the center weight and distribute the 
-   * negative remainder among the 8 neighbors to preserve overall brightness.
-   */
-  gfloat center_weight = 1.0f / fact;
-  gfloat neighbour_weight   = (1.0f - center_weight) / 8.0f;
-
+  GeglProperties     *o               = GEGL_PROPERTIES (op);
+  const Babl         *format          = babl_format_with_space ("R'G'B'A float",
+                                                                gegl_operation_get_source_space (op, "input"));
+  GeglRectangle       neighbor_rect   = *roi;
+  gfloat              percentage      = o->amount;
+  gfloat              fact            = 1.0f - (percentage / 100.0f);
+  gint                channels_num    = babl_format_get_n_components (format);
   GeglBufferIterator *iter;
+  gfloat              center_weight;
+  gfloat              neighbor_weight;
+
+  /* Clamping fact to avoid division by zero */
+  if (fact < 0.01f)
+    fact = 0.01f;
+
+  /* In the legacy version, the center was (800/fact) and neighbors were
+   * (pos - i*8)/8. In normalized float math, we set the center weight and
+   * distribute the negative remainder among the 8 neighbors to preserve
+   * overall brightness.
+   */
+  center_weight   = 1.0f / fact;
+  neighbor_weight = (1.0f - center_weight) / 8.0f;
+
   iter = gegl_buffer_iterator_new (output, roi, level, format,
                                    GEGL_ACCESS_WRITE,
-                                   GEGL_ABYSS_NONE, 1);
+                                   GEGL_ABYSS_NONE, 10);
+
+  /* top left */
+  neighbor_rect.x = roi->x - 1;
+  neighbor_rect.y = roi->y - 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* top center */
+  neighbor_rect.x = roi->x;
+  neighbor_rect.y = roi->y - 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* top right */
+  neighbor_rect.x = roi->x + 1;
+  neighbor_rect.y = roi->y - 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* center left */
+  neighbor_rect.x = roi->x - 1;
+  neighbor_rect.y = roi->y;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* center */
+  gegl_buffer_iterator_add (iter, input, roi, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* center right */
+  neighbor_rect.x = roi->x + 1;
+  neighbor_rect.y = roi->y;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* bottom left */
+  neighbor_rect.x = roi->x - 1;
+  neighbor_rect.y = roi->y + 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* bottom center */
+  neighbor_rect.x = roi->x;
+  neighbor_rect.y = roi->y + 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
+
+  /* bottom right */
+  neighbor_rect.x = roi->x + 1;
+  neighbor_rect.y = roi->y + 1;
+  gegl_buffer_iterator_add (iter, input, &neighbor_rect, level, format,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP);
 
   while (gegl_buffer_iterator_next (iter))
     {
-      GeglRectangle  out_roi = iter->items[0].roi;
-      gfloat        *output_data = iter->items[0].data;
+      gfloat *out_p = iter->items[0].data;
+      gfloat *tl    = iter->items[1].data;
+      gfloat *tc    = iter->items[2].data;
+      gfloat *tr    = iter->items[3].data;
+      gfloat *cl    = iter->items[4].data;
+      gfloat *cp    = iter->items[5].data;
+      gfloat *cr    = iter->items[6].data;
+      gfloat *bl    = iter->items[7].data;
+      gfloat *bc    = iter->items[8].data;
+      gfloat *br    = iter->items[9].data;
+      gint    i;
 
-      /* Expand ROI by 1 pixel in all directions to get the neighbors */ 
-      GeglRectangle input_roi;
-      input_roi.x      = out_roi.x - 1;
-      input_roi.y      = out_roi.y - 1;
-      input_roi.width  = out_roi.width  + 2;
-      input_roi.height = out_roi.height + 2;
-
-      gint    input_width = input_roi.width;
-      gfloat *input_buf   = g_new (gfloat, input_roi.width * input_roi.height * channels_num);
-
-      gegl_buffer_get (input, &input_roi, 1.0, format, input_buf,
-                       GEGL_AUTO_ROWSTRIDE, GEGL_ABYSS_CLAMP);
-
-      for (gint y = 0; y < out_roi.height; y++)
+      for (i = 0; i < iter->length; i++)
         {
-          for (gint x = 0; x < out_roi.width; x++)
+          gint c;
+
+          for (c = 0; c < channels_num - 1; c++)
             {
-              /* since input_buf starts at (out_roi.x - 1), our local 
-               * center in the buffer is at (x + 1, y + 1) */
-              gint in_x = x + 1;
-              gint in_y = y + 1;
+              gfloat neighbor_sum;
 
-              for (gint c = 0; c < 3; c++) /* Process R, G, B */
-                {
-                  gfloat res = 0.0f;
+              neighbor_sum = tl[c] + tc[c] + tr[c] +
+                             cl[c] +         cr[c] +
+                             bl[c] + bc[c] + br[c];
 
-                  /* 3x3 convolution loop */ 
-                  for (gint ky = -1; ky <= 1; ky++)
-                    {
-                      for (gint kx = -1; kx <= 1; kx++)
-                        {
-                          gint in_idx = ((in_y + ky) * input_width + (in_x + kx)) * channels_num + c;
-                          gfloat pixel_val = input_buf[in_idx];
-
-                          if (kx == 0 && ky == 0)
-                            res += pixel_val * center_weight; /* for center pixel */
-                          else
-                            res += pixel_val * neighbour_weight; /* for neighbours */
-                        }
-                    }
-                  
-                  gint out_idx = (y * out_roi.width + x) * channels_num + c;
-                  output_data[out_idx] = CLAMP (res, 0.0f, 1.0f);
-                }
-
-              /* copy Alpha channel without modification */ 
-              gint alpha_in  = (in_y * input_width + in_x) * channels_num + 3;
-              gint alpha_out = (y * out_roi.width + x) * channels_num + 3;
-              output_data[alpha_out] = input_buf[alpha_in];
+              out_p[c] = CLAMP ((cp[c] * center_weight) +
+                                (neighbor_sum * neighbor_weight),
+                                0.0f, 1.0f);
             }
+
+          /* copy alpha channel without modification */
+          out_p[3] = cp[3];
+
+          /* update to the next pixel location */
+          out_p += 4;
+          tl    += 4;
+          tc    += 4;
+          tr    += 4;
+          cl    += 4;
+          cp    += 4;
+          cr    += 4;
+          bl    += 4;
+          bc    += 4;
+          br    += 4;
         }
-      g_free (input_buf);
     }
 
   return TRUE;
@@ -129,15 +172,16 @@ gegl_op_class_init (GeglOpClass *klass)
   GeglOperationFilterClass *filter_class    = GEGL_OPERATION_FILTER_CLASS (klass);
 
   operation_class->prepare        = prepare;
-  operation_class->opencl_support = FALSE;
   filter_class->process           = process;
 
   gegl_operation_class_set_keys (operation_class,
-    "name",        "gegl:sharpen",
-    "title",       _("Sharpen (Legacy)"),
-    "categories",  "enhance",
-    "description", _("A port of the legacy GIMP sharpen filter to GEGL float math."),
-    NULL);
+                                 "name",        "gegl:sharpen",
+                                 "title",       _("Sharpen"),
+                                 "categories",  "enhance",
+                                 "description", _("Sharpens the image by enhancing "
+                                                  "edge contrast using a standard "
+                                                  "3x3 convolution kernel."),
+                                 NULL);
 }
 
 #endif
