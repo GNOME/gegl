935dcd34d0a036dd8e3618a13e178580ea5b7b5d
Ondřej Míchal <harrymichal@seznam.cz>
2026-05-24 23:36:01 +0300

    gegl/buffer: Clarify documentation of GeglBufferIterator
    This generally clarifies the wording in the whole documentation, adds
    documentation for GeglBufferIteratorItem, adjust the paragraphs to be a
    little more friendly based on how gi-docgen generates the HTML
    documentation and mainly adds a clarification on how ROIs of
    sub-iterators are handled.
    
    For example, it was not documented that the dimensions of the ROI of all
    subsequent iterators would be set to the dimensions of the first one.
    The only mention was that the chunks of memory would be aligned wrt the
    first one but how exactly was not clear. This came up while looking at
    the implementations of area filters which have to do some hacks to
    implement halos in sub-iterators. We want to support that case better,
    so let's document the current behaviour to have a better papertrail once
    we change this behaviour to support area filters better.

--- gegl/buffer/gegl-buffer-iterator.h
+++ gegl/buffer/gegl-buffer-iterator.h
@@ -31,13 +31,12 @@ G_BEGIN_DECLS
 typedef struct _GeglBufferIteratorPriv GeglBufferIteratorPriv;
 
 /***
- * GeglBufferIterator:
+ * GeglBufferIteratorItem:
+ * @data: a linear chunk of memory.
+ * @roi: a rectangle whose size is the number of pixels fitting into @data and
+ *   whose position is the offset wrt the original ROI of the iterator.
  *
- * [struct@Gegl.BufferIterator] allows to iterate over one or more
- * [class@Gegl.Buffer]s. In each iteration the new data is available as a linear
- * chunk of memory.
- *
- * See [ctor@Gegl.BufferIterator.new] and [method@Gegl.BufferIterator.next].
+ * A handle of a buffer sub-iterator managed by [struct@Gegl.BufferIterator].
  */
 
 typedef struct GeglBufferIteratorItem
@@ -46,9 +45,29 @@ typedef struct GeglBufferIteratorItem
   GeglRectangle roi;
 } GeglBufferIteratorItem;
 
+/***
+ * GeglBufferIterator:
+ * @length: the number of pixels in the linear chunk of memory fitting in the
+ *   first [struct@Gegl.BufferIteratorItem].
+ * @items: the list of all [struct@Gegl.BufferIteratorItem].
+ *
+ * [struct@Gegl.BufferIterator] allows to iterate over one or more
+ * [class@Gegl.Buffer]. In each iteration the new data is made available as a
+ * linear chunk of memory. For 2 and more buffers the linear chunks of memory
+ * are matched exactly in size and are aligned based on the offset between the
+ * ROI of the first buffer and the other buffer.
+ *
+ * For example, if [struct@Gegl.BufferIterator] is constructed with two
+ * sub-iterators where the second one has a larger ROI than the first, the
+ * content of the second buffer, in the area outside of the ROI of the first
+ * iterator, will not be iterated over.
+ *
+ * See [ctor@Gegl.BufferIterator.new] and [method@Gegl.BufferIterator.next].
+ */
+
 typedef struct GeglBufferIterator
 {
-  gint           length;
+  gint                    length;
   GeglBufferIteratorPriv *priv;
   GeglBufferIteratorItem  items[];
 } GeglBufferIterator;
@@ -56,71 +75,78 @@ typedef struct GeglBufferIterator
 
 /**
  * gegl_buffer_iterator_empty_new: (skip)
- * Create a new buffer iterator without adding any buffers.
+ * Create a new buffer iterator without adding any sub-iterators.
  *
- * Returns: a new buffer iterator.
+ * Returns: a new [struct@Gegl.BufferIterator] without any sub-iterators.
  */
 GeglBufferIterator *gegl_buffer_iterator_empty_new (int max_slots);
 
 /**
  * gegl_buffer_iterator_new: (skip)
- * @buffer: a #GeglBuffer
- * @roi: the rectangle to iterate over
- * @level: the level at which we are iterating, the roi will indicate the
- * extent at 1:1, x,y,width and height are/(2^level)
+ * @buffer: a [class@Gegl.Buffer] to iterate over
+ * @roi: the region in @buffer to iterate over
+ * @level: the level at which we are iterating. The @roi will indicate the
+ *   extent at 1:1. `x`, `y`, `width` and `height` are `/ (2^level)`
  * @format: the format we want to process this buffers data in, pass 0 to use
- * the buffers format.
+ *   the buffers format.
  * @access_mode: whether we need reading or writing to this buffer one of
  *   [flags@Gegl.AccessMode.READ], [flags@Gegl.AccessMode.WRITE] and
  *   [flags@Gegl.AccessMode.READWRITE].
- * @abyss_policy: how request outside the buffer extent are handled.
+ * @abyss_policy: how requests outside the buffer extent are handled.
+ * @max_slots: the maximum number of buffer iterators.
  *
- * Create a new buffer iterator, this buffer will be iterated through
- * in linear chunks, some chunks might be full tiles the coordinates, see
- * the documentation of [method@Gegl.BufferIterator.next] for how to use it and
- * destroy it.
+ * Creates a new [struct@Gegl.BufferIterator]. The iterator can use one or more
+ * sub-iterators to iterate over one or more [class@Gegl.Buffer].
+ * [struct@Gegl.BufferIterator] takes care of accessing the given buffers in
+ * the most optimal ways.
  *
- * Returns: a new buffer iterator that can be used to iterate through the
- * buffers pixels.
+ * See the documentation of [method.Gegl.BufferIterator.add] and
+ * [method.Gegl.BufferIterator.next].
+ *
+ * Returns: a new [struct@Gegl.BufferIterator] with one sub-iterator.
  */
-GeglBufferIterator * gegl_buffer_iterator_new  (
-                                                 GeglBuffer          *buffer,
-                                                 const GeglRectangle *roi,
-                                                 gint                 level,
-                                                 const Babl          *format,
-                                                 GeglAccessMode       access_mode,
-                                                 GeglAbyssPolicy      abyss_policy,
-                                                 gint                 max_slots);
-
+GeglBufferIterator * gegl_buffer_iterator_new  (GeglBuffer          *buffer,
+                                                const GeglRectangle *roi,
+                                                gint                 level,
+                                                const Babl          *format,
+                                                GeglAccessMode       access_mode,
+                                                GeglAbyssPolicy      abyss_policy,
+                                                gint                 max_slots);
 
 /**
  * gegl_buffer_iterator_add: (skip)
- * @iterator: a #GeglBufferIterator
- * @buffer: a #GeglBuffer
- * @roi: the rectangle to iterate over
- * @level: the level at which we are iterating, the roi will indicate the
- * extent at 1:1, x,y,width and height are/(2^level)
- * @format: the format we want to process this buffers data in, pass 0 to use the buffers format.
+ * @iterator: a [struct@Gegl.BufferIterator]
+ * @buffer: a [class@Gegl.Buffer] to iterate over
+ * @roi: the region in @buffer to iterate over
+ * @level: the level at which we are iterating. The @roi will indicate the
+ *   extent at 1:1. `x`, `y`, `width` and `height` are `/ (2^level)`
+ * @format: the format we want to process this buffers data in, pass 0 to use
+ *   the buffers format.
  * @access_mode: whether we need reading or writing to this buffer.
- * @abyss_policy: how request outside the buffer extent are handled.
+ * @abyss_policy: how requests outside the buffer extent are handled.
  *
- * Adds an additional buffer iterator that will be processed in sync with
- * the original one, if the buffer doesn't align with the other for tile access
- * the corresponding scans and regions will be serialized automatically using
- * [method@Gegl.Buffer.get].
+ * Adds an additional buffer sub-iterator into @iterator that will be processed
+ * in sync with the first one (unless this is the first sub-iterator).
  *
- * If the buffer shares its tiles with a previously-added buffer (in
- * particular, if the same buffer is added more than once), and at least one of
- * the buffers is accessed for writing, the corresponding iterated-over areas
- * should either completely overlap, or not overlap at all, in the coordinate-
- * system of the underlying tile storage (that is, after shifting each area by
- * the corresponding buffer's [property@Gegl.Buffer:shift-x] and
- * [property@Gegl.Buffer:shift-y] properties).  If the areas overlap, at most
- * one of the buffers may be accessed for writing, and the data pointers of the
- * corresponding iterator items may refer to the same data.
+ * The iterator prioritizes accessing tile data of @buffer directly but if the
+ * @buffer doesn't align with the first one for tile access the corresponding
+ * scans and regions will be serialized automatically using
+ * [method@Gegl.Buffer.get]. The @roi of all buffer sub-iterators added after
+ * the first one will have their dimensions set to the first one.
+ *
+ * If @buffer shares its tiles with a previously-added [class@Gegl.Buffer] (in
+ * particular, if the same [class@Gegl.Buffer] is added more than once), and at
+ * least one of the buffers is accessed for writing, the corresponding
+ * iterated-over areas should either completely overlap, or not overlap at all,
+ * in the coordinate system of the underlying tile storage (that is, after
+ * shifting each area by the corresponding buffer's
+ * [property@Gegl.Buffer:shift-x] and [property@Gegl.Buffer:shift-y]
+ * properties). If the areas overlap, at most one of the buffers may be
+ * accessed for writing, and the data pointers of the corresponding iterator
+ * items may refer to the same data.
  *
  * Returns: an integer handle refering to the indice in the iterator structure
- * of the added buffer.
+ * of the added sub-iterator.
  */
 gint                 gegl_buffer_iterator_add  (GeglBufferIterator  *iterator,
                                                 GeglBuffer          *buffer,
@@ -132,10 +158,9 @@ gint                 gegl_buffer_iterator_add  (GeglBufferIterator  *iterator,
 
 /**
  * gegl_buffer_iterator_stop: (skip)
- * @iterator: a GeglBufferIterator
+ * @iterator: a #GeglBufferIterator
  *
- * Cancels the current iteration, freeing up any temporary resources. The
- * iterator handle is no longer valid after invoking this function.
+ * Cancels the current iteration and invalidates the [struct@Gegl.BufferIterator].
  */
 void                 gegl_buffer_iterator_stop  (GeglBufferIterator *iterator);
 
@@ -143,11 +168,13 @@ void                 gegl_buffer_iterator_stop  (GeglBufferIterator *iterator);
  * gegl_buffer_iterator_next: (skip)
  * @iterator: a #GeglBufferIterator
  *
- * Do an iteration, this causes a new set of iterator->data[] to become
- * available if there is more data to process. Changed data from a previous
- * iteration step will also be saved now. When there is no more data to
- * be processed FALSE will be returned (and the iterator handle is no longer
- * valid).
+ * Do an iteration. If there is more data to process, a new set of linear
+ * chunks of memory becomes available in all @items via
+ * [struct@Gegl.BufferIteratorItem].
+ *
+ * Changed data from a previous iteration step will be saved now. When there
+ * is no more data to be processed FALSE will be returned (and the
+ * [struct@Gegl.BufferIterator] becomes invalid).
  *
  * Returns: TRUE if there is more work FALSE if iteration is complete.
  */
