d4c7bee97300e44ab8e5f8a887c68526bd6a8339
Ondřej Míchal <harrymichal@seznam.cz>
2026-06-04 02:29:24 +0300

    gegl/buffer: Respect ROI size diferences across subiterators
    Instead of overiding the whole iterated over area with the first
    iterator's ROI, grow/shrink the area of each sub-iterator based on the
    size difference between the sub-iterator and the first sub-iterator.
    
    With this, if an iterator has sub-iterators with differently sized ROIs,
    after each iteration the ROIs of each region are guaranteed to retain
    the same size difference between the original ROI of that sub-iterator
    and the first sub-iterator. For larger ROIs the abyss policy is taken
    into account for padding. For smaller ROIs the assurance is that they
    will always be at least one pixel tall and wide.
    
    This is especially useful for area filters where a single output pixel
    depends on some neighbourhood of input pixels. Most operations needing
    this kind of access are either allocating additional memory buffers or
    relying on the capability of the iterator to alias sub-iterators that
    share the same GeglBuffer.
    
    Making this change in a public object is possible because in prior GEGL
    releases the behaviour around mismatching (in size) ROIs was practically
    undefined in the documentation.
    
    Fixes https://gitlab.gnome.org/GNOME/gegl/-/work_items/467.
    
    [TODO: Port some ops and benchmark].
    [TODO: Add more tests (possibly parametrize as well). Especially for
    testing with smaller ROIs.].

---
+++ tests/buffer/tests/buffer_iterator_halo.c
@@ -0,0 +1,89 @@
+#include "babl/babl.h"
+
+#define KERNEL_WIDTH  3
+#define KERNEL_HEIGHT 3
+#define KERNEL_SIZE   KERNEL_WIDTH * KERNEL_HEIGHT
+#define SIZE_DIFF     42
+#define WIDTH_NEED    ((KERNEL_WIDTH  - 1) / 2)
+#define HEIGHT_NEED   ((KERNEL_HEIGHT - 1) / 2)
+
+TEST ()
+{
+  GeglBuffer         *out_buffer = NULL;
+  GeglBuffer         *in_buffer  = NULL;
+  GeglBufferIterator *iter       = NULL;
+  GeglRectangle       out_roi    = { 0, 0, 20, 20 };
+  GeglRectangle       in_roi     = {
+    -SIZE_DIFF,
+    -SIZE_DIFF,
+    out_roi.width + SIZE_DIFF * 2,
+    out_roi.height + SIZE_DIFF * 2,
+  };
+  gint                components = 0;
+
+  test_start();
+
+  g_assert_cmpint (KERNEL_WIDTH, <=, SIZE_DIFF);
+  g_assert_cmpint (KERNEL_HEIGHT, <=, SIZE_DIFF);
+
+  out_buffer = gegl_buffer_new (&out_roi, babl_format ("Y float"));
+  /* Uses out_roi on purpose. Relies on the abyss for the out-of-bounds halo
+   * values. */
+  in_buffer  = gegl_buffer_new (&out_roi, babl_format ("Y float"));
+  gegl_buffer_set_color (in_buffer, NULL, gegl_color_new ("white"));
+
+  iter = gegl_buffer_iterator_new (out_buffer, &out_roi, 0, NULL,
+                                   GEGL_ACCESS_WRITE, GEGL_ABYSS_NONE, 2);
+  gegl_buffer_iterator_add (iter, in_buffer, &in_roi, 0, NULL,
+                            GEGL_ACCESS_READ, GEGL_ABYSS_BLACK);
+
+  components = babl_format_get_n_components(gegl_buffer_get_format (in_buffer));
+
+  while (gegl_buffer_iterator_next (iter))
+    {
+      g_assert_true (gegl_rectangle_contains (&iter->items[1].roi,
+                                              &iter->items[0].roi));
+      g_assert_false (gegl_rectangle_equal (&iter->items[1].roi,
+                                            &iter->items[0].roi));
+      g_assert_true (iter->items[1].roi.width - iter->items[0].roi.width ==
+                     in_roi.width - out_roi.width);
+      g_assert_true (iter->items[1].roi.height - iter->items[0].roi.height ==
+                     in_roi.height - out_roi.height);
+
+      gfloat *out                 = iter->items[0].data;
+      gfloat *in                  = iter->items[1].data;
+      gfloat  kernel[KERNEL_SIZE] = { 0 };
+
+      for (gint i = 0; i < iter->length; i++)
+        {
+          gfloat val       = 0;
+          gint   in_offset = (iter->items[1].roi.width * (SIZE_DIFF - WIDTH_NEED))       +
+                             (SIZE_DIFF - HEIGHT_NEED)                                   +
+                             (iter->items[1].roi.width * (i / iter->items[0].roi.width)) +
+                             i % iter->items[0].roi.width;
+
+          for (int y = 0; y < KERNEL_HEIGHT; y++)
+            {
+              gint idx_y    = y * KERNEL_WIDTH;
+              gint offset_y = y * iter->items[1].roi.width * components;
+
+              for (int x = 0; x < KERNEL_WIDTH; x++)
+                {
+                  kernel[idx_y + x] = in[in_offset + offset_y + x * components];
+                }
+            }
+
+          for (gint j = 0; j < KERNEL_SIZE; j++)
+            val += kernel[j];
+
+          out[i] = val / (gfloat)(KERNEL_SIZE);
+        }
+    }
+
+  print_buffer (out_buffer);
+
+  g_object_unref (out_buffer);
+  g_object_unref (in_buffer);
+
+  test_end ();
+}
