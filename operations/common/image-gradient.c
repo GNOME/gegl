0fb57765c17c34c95b603e53a51d3b3bcf7669fe
Ondřej Míchal <harrymichal@seznam.cz>
2026-06-12 02:55:32 +0300

    operations: Use ROI halos in gegl:image-gradient

--- operations/common/image-gradient.c
+++ operations/common/image-gradient.c
@@ -40,6 +40,7 @@ property_enum (output_mode, _("Output mode"),
 #define GEGL_OP_NAME         image_gradient
 #define GEGL_OP_C_SOURCE     image-gradient.c
 
+#define KERNEL_SIZE 1
 #define POW2(x) ((x)*(x))
 
 #include "gegl-op.h"
@@ -56,7 +57,7 @@ prepare (GeglOperation *operation)
   area->left   =
   area->top    =
   area->right  =
-  area->bottom = 1;
+  area->bottom = KERNEL_SIZE;
 
   if (o->output_mode == GEGL_IMAGEGRADIENT_MAGNITUDE ||
       o->output_mode == GEGL_IMAGEGRADIENT_DIRECTION)
@@ -90,117 +91,75 @@ process (GeglOperation       *operation,
          const GeglRectangle *roi,
          gint                 level)
 {
-  GeglProperties  *o          = GEGL_PROPERTIES (operation);
-  const Babl      *in_format  = gegl_operation_get_format (operation, "input");
-  const Babl      *out_format = gegl_operation_get_format (operation, "output");
-  gfloat *row1;
-  gfloat *row2;
-  gfloat *row3;
-  gfloat *row4;
-  gfloat *top_ptr;
-  gfloat *mid_ptr;
-  gfloat *down_ptr;
-  gfloat *tmp_ptr;
-  gint    x, y;
-  gint    n_components;
+  const Babl     *in_format      = gegl_operation_get_format (operation, "input");
+  const Babl     *out_format     = gegl_operation_get_format (operation, "output");
+  const gint      in_components  = babl_format_get_n_components (in_format);
+  const gint      out_components = babl_format_get_n_components (out_format);
+  GeglProperties *o              = GEGL_PROPERTIES (operation);
+  GeglRectangle   in_roi         = {
+    roi->x - KERNEL_SIZE,
+    roi->y - KERNEL_SIZE,
+    roi->width  + KERNEL_SIZE * 2,
+    roi->height + KERNEL_SIZE * 2,
+  };
 
-  GeglRectangle row_rect;
-  GeglRectangle out_rect;
+  GeglBufferIterator *iter = gegl_buffer_iterator_new (
+      output, roi, 0, out_format, GEGL_ACCESS_WRITE, GEGL_ABYSS_NONE, 2);
+  gegl_buffer_iterator_add (iter, input, &in_roi, 0, in_format, GEGL_ACCESS_READ,
+                            GEGL_ABYSS_CLAMP);
 
-  n_components = babl_format_get_n_components (out_format);
-  row1 = g_new (gfloat, (roi->width + 2) * 3);
-  row2 = g_new (gfloat, (roi->width + 2) * 3);
-  row3 = g_new (gfloat, (roi->width + 2) * 3);
-  row4 = g_new0 (gfloat, roi->width * n_components);
-
-  top_ptr  = row1;
-  mid_ptr  = row2;
-  down_ptr = row3;
-
-  row_rect.width = roi->width + 2;
-  row_rect.height = 1;
-  row_rect.x = roi->x - 1;
-  row_rect.y = roi->y - 1;
-
-  out_rect.x      = roi->x;
-  out_rect.width  = roi->width;
-  out_rect.height = 1;
-
-  gegl_buffer_get (input, &row_rect, 1.0, in_format, top_ptr,
-                   GEGL_AUTO_ROWSTRIDE, GEGL_ABYSS_CLAMP);
-
-  row_rect.y++;
-  gegl_buffer_get (input, &row_rect, 1.0, in_format, mid_ptr,
-                   GEGL_AUTO_ROWSTRIDE, GEGL_ABYSS_CLAMP); 
-
-  for (y = roi->y; y < roi->y + roi->height; y++)
+  while (gegl_buffer_iterator_next (iter))
     {
-      row_rect.y = y + 1;
-      out_rect.y = y;
+      const gfloat *in  = iter->items[1].data;
+      gfloat       *out = iter->items[0].data;
 
-      gegl_buffer_get (input, &row_rect, 1.0, in_format, down_ptr,
-                       GEGL_AUTO_ROWSTRIDE, GEGL_ABYSS_CLAMP);
-
-      for (x = 1; x < row_rect.width - 1; x++)
+      for (gint y = 0; y < iter->items[0].roi.height; y++)
         {
-          gfloat dx[3];
-          gfloat dy[3];
-          gfloat magnitude[3];
-          gint   max_index;
+          gint y_offset = (y + KERNEL_SIZE) * iter->items[1].roi.width;
 
-          dx[0] = (mid_ptr[(x-1) * 3] - mid_ptr[(x+1) * 3]);
-          dy[0] = (top_ptr[x*3] - down_ptr[x*3]);
-          magnitude[0] = sqrtf(POW2(dx[0]) + POW2(dy[0]));
-
-          dx[1] = (mid_ptr[(x-1) * 3 + 1] - mid_ptr[(x+1) * 3 + 1]);
-          dy[1] = (top_ptr[x*3 + 1] - down_ptr[x*3 + 1]);
-          magnitude[1] = sqrtf(POW2(dx[1]) + POW2(dy[1]));
-
-          dx[2] = (mid_ptr[(x-1) * 3 + 2] - mid_ptr[(x+1) * 3 + 2]);
-          dy[2] = (top_ptr[x*3 + 2] - down_ptr[x*3 + 2]);
-          magnitude[2] = sqrtf(POW2(dx[2]) + POW2(dy[2]));
-
-          if (magnitude[0] > magnitude[1])
-            max_index = 0;
-          else
-            max_index = 1;
-
-          if (magnitude[2] > magnitude[max_index])
-            max_index = 2;
-
-          if (o->output_mode == GEGL_IMAGEGRADIENT_MAGNITUDE)
+          for (gint x = 0; x < iter->items[0].roi.width; x++)
             {
-              row4[(x-1) * n_components] = magnitude[max_index];
+              gfloat dx[3];
+              gfloat dy[3];
+              gfloat magnitude[3];
+              for (gint j = 0; j < in_components; j++)
+                {
+                  #define KERNEL(dx, dy, c) in[((y_offset + (dy * iter->items[1].roi.width)) + \
+                                                (KERNEL_SIZE + x + dx)) * in_components + c]
+                  dx[j] = KERNEL(-1, 0, j) - KERNEL(1, 0, j);
+                  dy[j] = KERNEL(0, -1, j) - KERNEL(0, 1, j);
+                  #undef KERNEL
+
+                  magnitude[j] = sqrtf(POW2(dx[j]) + POW2(dy[j]));
+              };
+
+              gint max_index = 0;
+              if (magnitude[0] <= magnitude[1])
+                max_index = 1;
+              if (magnitude[max_index] < magnitude[2])
+                max_index = 2;
+
+              if (o->output_mode == GEGL_IMAGEGRADIENT_MAGNITUDE ||
+                  o->output_mode == GEGL_IMAGEGRADIENT_BOTH)
+                {
+                  *out = magnitude[max_index];
+                }
+
+              if (o->output_mode == GEGL_IMAGEGRADIENT_DIRECTION ||
+                  o->output_mode == GEGL_IMAGEGRADIENT_BOTH)
+                {
+                  gfloat direction = atan2 (dy[max_index], dx[max_index]);
+
+                  if (o->output_mode == GEGL_IMAGEGRADIENT_DIRECTION)
+                    *out = direction;
+                  else
+                    *(out + 1) = direction;
+                }
+
+              out += out_components;
             }
-          else
-           {
-              gfloat direction = atan2 (dy[max_index], dx[max_index]);
-
-              if (o->output_mode == GEGL_IMAGEGRADIENT_DIRECTION)
-                {
-                  row4[(x-1) * n_components] = direction;
-                }
-              else
-                {
-                  row4[(x-1) * n_components] = magnitude[max_index];
-                  row4[(x-1) * n_components + 1] = direction;
-                }
-           }
         }
-
-      gegl_buffer_set (output, &out_rect, level, out_format, row4,
-                       GEGL_AUTO_ROWSTRIDE);
-
-      tmp_ptr = top_ptr;
-      top_ptr = mid_ptr;
-      mid_ptr = down_ptr;
-      down_ptr = tmp_ptr;
     }
-
-  g_free (row1);
-  g_free (row2);
-  g_free (row3);
-  g_free (row4);
 
   return TRUE;
 }
@@ -208,16 +167,13 @@ process (GeglOperation       *operation,
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
-  GeglOperationClass       *operation_class;
-  GeglOperationFilterClass *filter_class;
+  GeglOperationClass       *operation_class = GEGL_OPERATION_CLASS (klass);
+  GeglOperationFilterClass *filter_class    = GEGL_OPERATION_FILTER_CLASS (klass);
 
-  operation_class = GEGL_OPERATION_CLASS (klass);
-  filter_class    = GEGL_OPERATION_FILTER_CLASS (klass);
-
-  filter_class->process             = process;
   operation_class->prepare          = prepare;
   operation_class->get_bounding_box = get_bounding_box;
-  operation_class->opencl_support   = FALSE;
+
+  filter_class->process = process;
 
   gegl_operation_class_set_keys (operation_class,
     "name",        "gegl:image-gradient",
